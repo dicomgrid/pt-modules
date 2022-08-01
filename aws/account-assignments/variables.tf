6f3d8f818b5e0d06f3ac45d8023787a217496cf9
anknighton <97533898+anknighton@users.noreply.github.com>
2022-08-01 13:07:11 -0400

    Update variables.tf

--- aws/account-assignments/variables.tf
+++ aws/account-assignments/variables.tf
@@ -1,6 +1,6 @@
 variable "account_assignments" {
   type = list(object({
-    account             = string
+    account             = list
     permission_set_name = string
     permission_set_arn  = string
     principal_name      = string
