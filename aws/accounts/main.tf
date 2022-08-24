93ace976f3ec2c2d8d254b56efda5051803a54fb
aknighton <andrew.knighton@intelerad.com>
2022-08-24 10:18:39 -0400

    aws-account module update

--- aws/accounts/main.tf
+++ aws/accounts/main.tf
@@ -4,7 +4,7 @@ resource "aws_organizations_account" "account" {
   email                      = each.value.email
   role_name                  = each.value.role
   iam_user_access_to_billing = var.access-billing
-  parent_id                  = aws_organizations_organizational_unit.org[each.value.environment].id
+  parent_id                  = each.value.parent_id
 
   lifecycle {
     ignore_changes = [role_name, iam_user_access_to_billing]
