00a50c32d57f27e6dc83ac199a86e6576344d0db
aknighton <andrew.knighton@intelerad.com>
2022-08-24 11:13:12 -0400

    v3

--- aws/accounts/main.tf
+++ aws/accounts/main.tf
@@ -5,7 +5,7 @@ resource "aws_organizations_account" "account" {
   role_name                  = each.value.role
   iam_user_access_to_billing = var.access_billing
   parent_id                  = var.parent_id[each.value.parent_id]
-  tags = var.tags[each.value.tags]
+  tags = each.value.tags
   lifecycle {
     ignore_changes = [role_name, iam_user_access_to_billing]
   }
