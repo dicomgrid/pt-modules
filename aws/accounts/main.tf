3dcbf5079eaec0ac0526faaeea0a4c854390c210
anknighton <97533898+anknighton@users.noreply.github.com>
2022-08-24 10:41:02 -0400

    Update main.tf

--- aws/accounts/main.tf
+++ aws/accounts/main.tf
@@ -4,7 +4,7 @@ resource "aws_organizations_account" "account" {
   email                      = each.value.email
   role_name                  = each.value.role
   iam_user_access_to_billing = var.access_billing
-  parent_id                  = each.value.parent_id
+  parent_id                  = var.parent_id[each.value.parent_id]
 
   lifecycle {
     ignore_changes = [role_name, iam_user_access_to_billing]
