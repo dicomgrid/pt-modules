21e1c44df4348b82490511c61da77735fbf5acb7
Cody Williams <cody.williams@intelerad.com>
2022-08-24 13:08:03 -0500

    add tags dynamic block

--- aws/accounts/main.tf
+++ aws/accounts/main.tf
@@ -5,7 +5,16 @@ resource "aws_organizations_account" "account" {
   role_name                  = each.value.role
   iam_user_access_to_billing = var.access_billing
   parent_id                  = var.parent_id[each.value.parent_id]
-  tags = var.tags[each.value.tags]
+
+  dynamic "tags" {
+    for_each                   = each.value.tags
+    content {
+      name                  = lookup(each.value, "name" , null)
+      department            = lookup(each.value, "department" , null)
+      ambra-storage         = lookup(each.value, "ambra-storage" , null)
+    }
+  }
+
   lifecycle {
     ignore_changes = [role_name, iam_user_access_to_billing]
   }
