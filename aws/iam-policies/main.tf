4a445be8f7f0c694250f5b770ba8a08591bf09a7
Cody Williams <cody.williams@intelerad.com>
2022-08-29 01:07:55 -0500

    update amzn2 lookup; correct policy var

--- aws/iam-policies/main.tf
+++ aws/iam-policies/main.tf
@@ -1,11 +1,10 @@
-resource "aws_iam_instance_profile" "main" {
-  name = var.name
-  role = var.role 
-}
-
 resource "aws_iam_role" "main" {
   name               = var.name
   assume_role_policy = var.assume_role_policy
+}
+resource "aws_iam_instance_profile" "main" {
+  name = var.name
+  role = aws_iam_role.main.id
 }
 
 resource "aws_iam_role_policy" "main" {
