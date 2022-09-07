b8ddab6a841ac430ad4e5e391bd6790f312e25c5
Cody Williams <cody.williams@intelerad.com>
2022-09-07 00:28:52 -0500

    bypass subnet lookup to use specified subnets

--- aws/load-balancer/main.tf
+++ aws/load-balancer/main.tf
@@ -5,8 +5,7 @@ resource "aws_lb" "main" {
   load_balancer_type               = var.load_balancer_type
   name                             = var.name
   security_groups                  = var.load_balancer_type == "application" ? data.aws_security_groups.main[0].ids : null
-  subnets                          = data.aws_subnets.main.ids
-
+  subnets                          = var.subnets
   tags = {
     Application = var.application
     Billing     = var.billing
