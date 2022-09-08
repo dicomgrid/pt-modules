6a9fcd0d747d9926ec6c3ce1fcba257121149e17
acarter-ims <austin.carter+gh@intelerad.com>
2022-09-08 06:53:45 -0500

    Fixed providers fixed providers

--- aws/acm-pki/providers.tf
+++ aws/acm-pki/providers.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_version = ">= 1.0.9"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.29.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
     }
   }
 }
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
