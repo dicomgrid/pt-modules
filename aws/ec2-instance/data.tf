e30c4994cefcc5932c6983f4af8a0ed6bb6238c6
acarter-ims <austin.carter@intelerad.com>
2022-09-16 13:40:46 -0500

    removing amazon linux

--- aws/ec2-instance/data.tf
+++ aws/ec2-instance/data.tf
@@ -23,30 +23,6 @@ data "aws_ami" "centos_7" {
   }
 }
 
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-}
-
 data "aws_ami" "ubuntu_22_04" {
   most_recent = true
   owners      = ["099720109477"]
