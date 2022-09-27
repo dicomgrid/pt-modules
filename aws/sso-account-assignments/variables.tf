29a78fb40f95510ccd5ac7be98692becc9644c4d
skylar.houghton <skylar.houghton@gmail.com>
2022-09-27 11:20:37 -0500

    PLT-103: Renamed identity-center modules

--- aws/sso-account-assignments/variables.tf
+++ aws/sso-account-assignments/variables.tf
@@ -1,9 +1,5 @@
-variable "account_assignments" {
-  type = list(object({
-    account             = string
-    permission_set_name = string
-    permission_set_arn  = string
-    principal_name      = string
-    principal_type      = string
-  }))
+variable "permset_name" {}
+variable "group" {}
+variable "account_list" {
+  default = ([])
 }
