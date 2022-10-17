b545436676732f5e65c7c9acc1f25820e7d66e40
aknighton <andrew.knighton@intelerad.com>
2022-10-17 12:58:22 -0400

    OPS-1893: adding role for s3objects

--- aws/lambda-s3-bucket-iam-role/main.tf
+++ aws/lambda-s3-bucket-iam-role/main.tf
@@ -24,6 +24,30 @@ resource "aws_iam_role" "s3_bucket_manager_main" {
   ]
 }
 
+# Create the S3 Object Manager role that can carry out the S3 Object Management policy
+resource "aws_iam_role" "s3_bucket_manager_main" {
+  depends_on = [
+    aws_iam_policy.s3_object_management_main
+  ]
+  name     = "S3objectManager"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "AWS": "arn:aws:iam::${var.primary_account}:role/${var.instance_role}"
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    aws_iam_policy.s3_object_management_main.arn
+  ]
+}
 
 # Create the S3 Bucket Management policy that allows the lambda to create buckets once it assumes the S3BucketManager role needs to be on each storage node
 resource "aws_iam_policy" "s3_bucket_management_main" {
@@ -79,3 +103,76 @@ resource "aws_iam_policy" "s3_bucket_management_main" {
   })
 }
 
+
+
+# Create the S3 Object Management policy that allows the storage engines to manage objects once it assumes the S3ObjectManager role. 
+resource "aws_iam_policy" "s3_object_management_main" {
+
+  name        = "S3ObjectManagement"
+  description = "Allow storage engines access to s3 objects"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "S3ObjectManagement",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:DeleteObjectVersion",
+          "s3:GetObjectVersionTagging",
+          "s3:RestoreObject",
+          "s3:GetObjectVersionAttributes",
+          "s3:ReplicateObject",
+          "s3:GetObjectVersionTorrent",
+          "s3:GetObjectAcl",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectTagging",
+          "s3:PutObjectTagging",
+          "s3:DeleteObject",
+          "s3:PutObjectAcl",
+          "s3:DeleteObjectTagging",
+          "s3:GetObjectRetention",
+          "s3:GetObjectAttributes",
+          "s3:PutObjectVersionTagging",
+          "s3:PutObjectLegalHold",
+          "s3:InitiateReplication",
+          "s3:DeleteObjectVersionTagging",
+          "s3:GetObjectLegalHold",
+          "s3:ListMultipartUploadParts",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectTorrent",
+          "s3:PutObjectRetention",
+          "s3:PutObjectVersionAcl",
+          "s3:GetObjectVersionForReplication",
+          "s3:ReplicateDelete",
+          "s3:GetObjectVersion",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:GetLifecycleConfiguration",
+          "s3:ListBucketMultipartUploads",
+          "s3:GetBucketTagging",
+          "s3:GetInventoryConfiguration",
+          "s3:GetBucketWebsite",
+          "s3:ListBucketVersions",
+          "s3:GetBucketLogging",
+          "s3:ListBucket",
+          "s3:GetBucketVersioning",
+          "s3:GetBucketAcl",
+          "s3:GetBucketNotification",
+          "s3:GetBucketPolicy",
+          "s3:GetReplicationConfiguration",
+          "s3:GetBucketObjectLockConfiguration",
+          "s3:GetEncryptionConfiguration",
+          "s3:GetIntelligentTieringConfiguration",
+          "s3:GetBucketRequestPayment",
+          "s3:GetBucketCORS",
+          "s3:GetMetricsConfiguration",
+          "s3:GetBucketOwnershipControls",
+          "s3:GetBucketLocation"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
