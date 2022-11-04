05e540c4511d6470b9f39f0a51a0f7d9d0baab5f
Cody Williams <cody.williams@intelerad.com>
2022-11-04 15:36:56 -0500

    update logic for optional role_policy

--- aws/iam-instance-profile/main.tf
+++ aws/iam-instance-profile/main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_instance_profile" "main" {
 }
 
 resource "aws_iam_role_policy" "main" {
-  for_each = var.policy == null ? [0] : [1]
+  for_each = var.policy == null ? [] : [1]
   name   = var.name
   role   = aws_iam_role.main.id
   policy = var.policy
