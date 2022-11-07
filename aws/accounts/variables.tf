f41afcb9aca0ac9f25a8fb1a3d4ab4246030ec9f
aknighton <andrew.knighton@intelerad.com>
2022-11-07 11:58:23 -0500

    PLT-184: setting defaults

--- aws/accounts/variables.tf
+++ aws/accounts/variables.tf
@@ -1,11 +1,11 @@
 locals {
   # Get account info from data block
-  ou                  = data.aws_organizations_organizational_units.main.children[*]
+  ou = data.aws_organizations_organizational_units.main.children[*]
   # Create a map of child OU names and IDs from the accounts variable
-  ou_map               = { for ou in local.ou : ou.name => ou.id }
+  ou_map = { for ou in local.ou : ou.name => ou.id }
   # Translate account names in permsets.[permset]accounts to accounts IDs and create dictionary of all IDs 
   #permset_account_ids = { for name in keys(var.permsets) : name => [for account in var.permsets[name].accounts : local.account_map[account]] }
-  ou_ids = { for name in ou_name : name =>  local.ou_map[name] }
+  ou_ids = { for name in ou_name : name => local.ou_map[name] }
 }
 
 
@@ -44,7 +44,11 @@ variable "parent_id_map" {
 # Input Variables
 variable "name" {}
 variable "email" {}
-variable "role" {}
+variable "role" {
+  default = ""
+}
 variable "ou_name" {}
-variable "tags" {}
+variable "tags" {
+  default = ""
+}
 
