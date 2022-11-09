8144f7593d1d7e3f784fcbacf7c964203f846abf
acarter-ims <austin.carter@intelerad.com>
2022-11-09 08:49:31 -0600

    Update variables.tf

--- aws/vpc-standard/variables.tf
+++ aws/vpc-standard/variables.tf
@@ -1,25 +1,26 @@
-variable "region" {
-  default = "us-west-1"
-}
-
 variable "environment" {
   description = "Deployment Environment"
-  default ="development"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block of the vpc"
-  default     = "10.138.0.0/23"
 }
 
 variable "public_subnets_cidr" {
-  type        = list(any)
+  type        = list
   description = "CIDR block for Public Subnet"
-  default     = ["10.138.1.0/26"]
 }
 
 variable "private_subnets_cidr" {
-  type        = list(any)
+  type        = list
   description = "CIDR block for Private Subnet"
-  default     = ["10.138.0.0/26"]
+}
+
+variable "region" {
+  description = "Region in which the bastion host will be launched"
+}
+
+variable "availability_zones" {
+  type        = list
+  description = "AZ in which all the resources will be deployed"
 }
