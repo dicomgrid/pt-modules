6ffee26cdacff8c649fa4e2fb5fadb70835f005c
acarter-ims <austin.carter@intelerad.com>
2022-11-09 08:37:39 -0600

    adding module

--- aws/vpc-standard/main.tf
+++ aws/vpc-standard/main.tf
@@ -1,91 +1,145 @@
-resource "aws_vpc" "main" {
-vpc_cidr_block = var.vpc_ip
-instance_tenancy = var.tenancy
-  
-  enable_dns_hostnames = var.dns_hostnames
-  enable_dns_support = var.dns_support
- 
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
-    Name = ""
-    Environment = var.Environment
-    Owner = var.Owner
-    Creator = var.Creator
-    
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
   }
 }
 
-resource "aws_eip" "eip_natgw" {
-  vpc      = true
-}
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+# Subnets
+# Internet Gateway for Public Subnet
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = ""
-    Environment = "development" 
-    Owner = "Platform" 
-    Creator = "Terraform"
-}
-}
-
-
-resource "aws_route_table_association" "a" {
-  count = length(aws_subnet.public_subnets)
-  subnet_id      = element(aws_subnet.public_subnets[*].id,count.index)
-  route_table_id = aws_route_table.igw_rt.id
-}
-
-locals {
-  availability_zone_names = "${data.aws_availability_zones.availability_zones.names}"
-  max_subnets = 2
-}
-
-resource "aws_subnet" "public_subnets" {
-  vpc_id = "${aws_vpc.main.id}"
-  count = "${length(local.availability_zone_names)}"
-  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)}"
-  availability_zone = "${local.availability_zone_names[count.index]}"
-  map_public_ip_on_launch = var.public_ip_on_launch
-  tags = {
-    Name = "public.${local.availability_zone_names[count.index]}"
-    Environment = var.Environment
-    Owner = var.Owner
-    Creator = var.Creator
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
   }
 }
-resource "aws_subnet" "private_subnets" {
-  vpc_id = "${aws_vpc.main.id}"
-  count = "${length(local.availability_zone_names)}"
-  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 4, count.index+2)}"
-  availability_zone = "${local.availability_zone_names[count.index]}"
+
+# Elastic-IP (eip) for NAT *
+/*
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.id]
+}*/
+
+# NAT
+/*
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+
+  tags = {
+    Name        = "nat"
+    Environment = "${var.environment}"
+  }
+}*/
+
+# Public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.public_subnets_cidr)
+  cidr_block              = element(var.public_subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-public-subnet"
+    Environment = "${var.environment}"
+  }
+}
+
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.private_subnets_cidr)
+  cidr_block              = element(var.private_subnets_cidr, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "private.${local.availability_zone_names[count.index]}"
-    Environment = var.Environment
-    Owner = var.Owner
-    Creator = var.Creator
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+    Environment = "${var.environment}"
   }
 }
-resource "aws_route_table" "igw_rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+
+
+# Routing tables to route traffic for Private Subnet
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
   tags = {
-    Name = "igw_rt_primary"
-    Environment = var.Environment
-    Owner = var.Owner
-    Creator = var.Creator
+    Name        = "${var.environment}-private-route-table"
+    Environment = "${var.environment}"
   }
 }
 
+# Routing tables to route traffic for Public Subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
 
-resource "aws_nat_gateway" "natgw_default" {
-  allocation_id = aws_eip.eip_natgw.id
-  subnet_id     = aws_subnet.private_subnets[0].id
-    tags = {
-    Name = "nat-gw primary"
+  tags = {
+    Name        = "${var.environment}-public-route-table"
+    Environment = "${var.environment}"
   }
-   depends_on = [aws_internet_gateway.igw]
+}
+
+# Route for Internet Gateway
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
+
+# Route for NAT 
+/*
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+} */
+
+# Route table associations for both Public & Private Subnets
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_cidr)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_cidr)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+# Default Security Group of VPC
+resource "aws_security_group" "default" {
+  name        = "${var.environment}-default-sg"
+  description = "Default SG to alllow traffic from the VPC"
+  vpc_id      = aws_vpc.vpc.id
+  depends_on = [
+    aws_vpc.vpc
+  ]
+
+    ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = "true"
+  }
+
+  tags = {
+    Environment = "${var.environment}"
+  }
 }
