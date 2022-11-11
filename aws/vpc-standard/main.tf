6b1207a6ebd23fd707c2fb965916e4dd5592058a
acarter-ims <austin.carter@intelerad.com>
2022-11-11 17:29:16 -0600

    readding output

--- aws/vpc-standard/main.tf
+++ aws/vpc-standard/main.tf
@@ -1,15 +1,13 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_range
+  cidr_block=   var.cidr
   instance_tenancy = var.tenancy
-
   enable_dns_hostnames = var.dns_hostnames
   enable_dns_support = var.dns_support
   tags = {
-    Name = "$(Owner)_vpc"
-    Environment = var.Environment
     Owner = var.Owner
+    Environment = var.Environment
     Creator = var.Creator
-    
+  
   }
 }
 
@@ -18,7 +16,7 @@ locals {availability_zone_names = "${data.aws_availability_zones.availability_zo
 resource "aws_subnet" "public_subnets" {
   vpc_id = "${aws_vpc.main.id}"
   count = length(data.aws_availability_zones.availability_zones.names) < 3 ? length(data.aws_availability_zones.availability_zones.names) : 2
-  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 2, count.index)}"
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 3, count.index+1)}"
   availability_zone = "${local.availability_zone_names[count.index]}"
   map_public_ip_on_launch = true
   tags = {
@@ -32,7 +30,7 @@ resource "aws_subnet" "public_subnets" {
 resource "aws_subnet" "private_subnets" {
   vpc_id = "${aws_vpc.main.id}"
   count = length(data.aws_availability_zones.availability_zones.names) < 3 ? length(data.aws_availability_zones.availability_zones.names) : 2
-  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 2, count.index)}"
+  cidr_block = "${cidrsubnet(aws_vpc.main.cidr_block, 3, count.index)}"
   availability_zone = "${local.availability_zone_names[count.index]}"
 
   map_public_ip_on_launch = false
