afd342c324fddba4053ff84a643edaca777abe75
aknighton <andrew.knighton@intelerad.com>
2022-11-21 10:32:51 -0500

    OPS-1963: fixing

--- aws/identity-center/data.tf
+++ aws/identity-center/data.tf
@@ -9,7 +9,7 @@ data "aws_identitystore_group" "group" {
   identity_store_id = tolist(data.aws_ssoadmin_instances.instance.identity_store_ids)[0]
 
   alternate_identifier{
-    filter {
+    unique_attribute {
       attribute_path  = "DisplayName"
       attribute_value = var.group
   }
