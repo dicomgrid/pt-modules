4557439992632d4b8272167240e8ec3da318838b
aknighton <andrew.knighton@intelerad.com>
2022-12-16 14:51:21 -0500

    PLT-354: changing service instance profile doc

--- aws/lambda-s3-bucket-provisioning/data.tf
+++ aws/lambda-s3-bucket-provisioning/data.tf
@@ -177,7 +177,7 @@ data "aws_iam_policy_document" "s3-bucket-provisioning-instance-profile" {
   provider = aws.primary
   statement {
     actions   = ["lambda:InvokeFunction"]
-    resources = ["arn:aws:lambda:${var.aws_region}:${var.primary_account}:function:s3-bucket-provisioning"]
+    resources = ["arn:aws:lambda:${var.aws_region}:${var.primary_account}:function:${aws_lambda_function.s3_bucket_provisioning.function_name}"]
   }
 
   statement {
@@ -191,7 +191,10 @@ data "aws_iam_policy_document" "s3-bucket-provisioning-instance-profile" {
     condition {
       test     = "ArnEquals"
       variable = "lambda:FunctionArn"
-      values   = ["arn:aws:lambda:${var.aws_region}:${var.primary_account}:function:s3-bucket-provisioning"]
+      values   = [
+        "arn:aws:lambda:${var.aws_region}:${var.primary_account}:function:${aws_lambda_function.s3_bucket_provisioning.function_name}:*",
+        "arn:aws:lambda:${var.aws_region}:${var.primary_account}:function:${aws_lambda_function.s3_bucket_provisioning.function_name}"
+        ]
     }
   }
   statement {
