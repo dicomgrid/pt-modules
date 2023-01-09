bd40be3da46d8ac17799252a13b13f5dfb3d0e2f
skylar.houghton <skylar.houghton@gmail.com>
2023-01-09 17:14:09 -0600

    PLT-217: Updated pb config

--- aws/ssm-patch-baseline-centos/main.tf
+++ aws/ssm-patch-baseline-centos/main.tf
@@ -3,4 +3,29 @@ resource "aws_ssm_patch_baseline" "pb-linux" {
   description      = var.description
   operating_system = "CENTOS"
   rejected_patches = [jsonencode(var.rejected_patches)]
+
+  global_filter {
+    key = "CLASSIFICATION"
+    values = [
+      "Security",
+      "Bugfix",
+      "Recommended"
+    ]
+  }
+
+  approval_rule {
+    approve_after_days = 7
+    compliance_level = "CRITICAL"
+    patch_filter {
+      key = "CLASSIFICATION"
+      values = var.centos_patch_classifications
+    }
+    patch_filter {
+      key = "SEVERITY"
+      values = ["Critical"]
+    }
+    #TODO: Add Medium Severity Level
+    #TODO: Add Low Severity Level
+  }
+
 }
--- aws/ssm-patch-baseline-centos/variables.tf
+++ aws/ssm-patch-baseline-centos/variables.tf
@@ -14,3 +14,8 @@ variable "rejected_patches" {
   type    = list(string) #Example syntax: ["systemd", "corosync"]
   default = []
 }
+
+variable "centos_patch_classifications" {
+  type = list(string)
+  default = ["Security", "Bugfix", "Recommended"]
+}
