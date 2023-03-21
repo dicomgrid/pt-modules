fbb942c04690b74db7e3c544715c0a263980a834
cwilliams-intelerad <81389845+cwilliams-intelerad@users.noreply.github.com>
2023-03-21 12:54:01 -0500

    PLT-772: decouple identity center (#107)
    ## Submission type
    <!--- Issue Type -->
    - [ ] Bug fix/cleanup (non-breaking change which fixes known
    deficiencies)
    - [ ] New feature (non-breaking change which adds functionality)
    - [x] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    #Please verify there is a corresponding jira task for this commit.

--- aws/identity-center/account-assignments/main.tf
+++ aws/identity-center/account-assignments/main.tf
@@ -1,9 +1,9 @@
 resource "aws_ssoadmin_account_assignment" "assignment" {
   for_each           = local.account_bindings_dict
-  instance_arn       = local.permset_data[each.key].instance_arn
-  permission_set_arn = local.permset_data[each.key].arn
-  principal_id       = local.group_data[each.key].id
+  instance_arn       = var.sso_instance_arn
+  permission_set_arn = var.permsets_map[each.value.permset].arn
+  principal_id       = var.groups_map[each.value.group].id
   principal_type     = "GROUP"
   target_type        = "AWS_ACCOUNT"
-  target_id          = local.accounts_id_map[var.account_name]
+  target_id          = var.accounts_id_map[var.account_name]
 }
