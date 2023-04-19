ba16821a36acf5810d477dd34882f105902ce4a3
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-19 16:25:46 -0500

    PLT-887: update r53 to use local tags

--- aws/route53/main.tf
+++ aws/route53/main.tf
@@ -1,11 +1,4 @@
 resource "aws_route53_zone" "zone" {
   name = var.zone
-
-  tags = {
-    name        = var.zone
-    environment = var.environment
-    billing     = var.billing
-    creator     = var.creator
-  }
-
+  tags = local.tags
 }
