95a8f7058e5ac548bdedaa13972f9fcb9b63f591
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-19 16:38:59 -0500

    PLT-887: update acm providers

--- aws/acm/providers.tf
+++ aws/acm/providers.tf
@@ -1,13 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.30.0"
+      version = ">= 4.50.0"
     }
   }
-}
-
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
 }
