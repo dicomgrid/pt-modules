7747d44c275192c6df1fc9a5bb92acc639a48dc5
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-21 02:26:45 -0500

    PLT-887: update common outputs for ecr policies

---
+++ aws/common/accounts/output.tf
@@ -0,0 +1,4 @@
+output "data" { value = local.accounts_data }
+output "id_map" { value = local.accounts_id_map }
+output "all" { value = local.accounts_all }
+output "group" { value = local.accounts_group }
--- aws/common/accounts/variables.tf
+++ aws/common/accounts/variables.tf
@@ -5,10 +5,21 @@ locals {
   accounts_id_map = { for account in local.accounts_data : account.name => account.id }
   # List of all account names that are active
   accounts_all = [for account in local.accounts_data : account.name if account.status == "ACTIVE"]
-  # Lists of all account names for ambra storage accounts
-  accounts_ambra_storage_dev = [for account in local.accounts_all : account if length(regexall("Storage.*_Dev", account)) > 0]
-  accounts_ambra_storage_prod = [for account in local.accounts_all : account if length(regexall("storage.*_prod", account)) > 0]
-  accounts_ambra_storage_uat = [for account in local.accounts_all : account if length(regexall("Storage.*_UAT", account)) > 0]
-  # Lists of all account names for platform accounts
-  accounts_platform= [for account in local.accounts_all : account if length(regexall("ims-platform*", account)) > 0]
+  accounts_group = {
+    # Lists of all account names for ambra storage accounts
+    ambra_storage_dev = [for account in local.accounts_all : account if length(regexall("Storage.*_Dev", account)) > 0]
+    ambra_storage_prod = [for account in local.accounts_all : account if length(regexall("storage.*_prod", account)) > 0]
+    ambra_storage_uat = [for account in local.accounts_all : account if length(regexall("Storage.*_UAT", account)) > 0]
+    # Lists of all account names for cloudpacs accounts
+    cloudpacs = concat(
+        [for account in local.accounts_all : account if length(regexall("intelerad-pacs*", account)) > 0], 
+        [for account in local.accounts_all : account if length(regexall("intelerad-cloudpacs*", account)) > 0]
+        )
+    # Lists of all account names for cloudinfra accounts
+    cloudinfra = [for account in local.accounts_all : account if length(regexall("intelerad-cloudinfra*", account)) > 0]
+    # Lists of all account names for platform accounts
+    iws = [for account in local.accounts_all : account if length(regexall("intelerad-iws*", account)) > 0]
+    # Lists of all account names for platform accounts
+    platform = [for account in local.accounts_all : account if length(regexall("ims-platform*", account)) > 0]
+  }
 }
