9e642d54f1e75729b2d6e7a416a39e60f4453e5c
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-21 16:49:37 -0500

    PLT-887: create key policy resource

--- aws/kms-key/main.tf
+++ aws/kms-key/main.tf
@@ -1,11 +1,16 @@
 resource "aws_kms_key" "main" {
   description             = var.description
   deletion_window_in_days = var.deletion_window_in_days
-  policy = var.kms_policy_enabled ? local.kms_policy : null
   tags = local.tags
 }
 
 resource "aws_kms_alias" "main" {
   name          = var.kms_alias == null ? "alias/${var.name}" : var.kms_alias
   target_key_id = aws_kms_key.main.key_id
+}
+
+resource "aws_kms_key_policy" "main" {
+  count = var.kms_policy_enabled ? 1 : 0
+  key_id = aws_kms_key.main.id
+  policy = local.kms_policy
 }
