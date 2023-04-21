034f640a4b627a7a58f051ff76bd24acc3134871
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-21 15:49:45 -0500

    PLT-887: update kms module for bs

---
+++ aws/kms-key/kms-policy-default.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "kms_policy" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow s3 event notification"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["kms:GenerateDataKey", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
--- aws/kms-key/main.tf
+++ aws/kms-key/main.tf
@@ -2,12 +2,10 @@ resource "aws_kms_key" "main" {
   description             = var.description
   deletion_window_in_days = var.deletion_window_in_days
 
-  tags = {
-    Name = "${var.name}"
-  }
+  tags = local.tags
 }
 
 resource "aws_kms_alias" "main" {
-  name          = "alias/${var.name}"
+  name          = var.kms_alias == null ? "alias/${var.name}" : var.kms_alias
   target_key_id = aws_kms_key.main.key_id
 }
