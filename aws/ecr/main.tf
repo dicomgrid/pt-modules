1c6ba27f1154bce019bc3a2fbc7d7a84ba010a37
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-20 23:33:42 -0500

    PLT-887: Add ecr dependancies

--- aws/ecr/main.tf
+++ aws/ecr/main.tf
@@ -3,18 +3,23 @@ resource "aws_ecr_repository" "main" {
   image_tag_mutability = var.image_tag_mutability
 
   encryption_configuration {
-    encryption_type = var.encryption_type
+    encryption_type = var.kms_key == null ? "AES256" : "KMS"
+    kms_key         = var.kms_key
   }
-
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
-
   tags = local.tags
 }
 
 resource "aws_ecr_lifecycle_policy" "main" {
-  count = var.lifecycle_policy == null ? 0 : 1
-  repository = var.name
-  policy = var.policy
+  count = var.lifecycle_policy_enable == true ? 1 : 0
+  repository = aws_ecr_repository.main.name
+  policy = jsonencode(local.lifecycle_policy)
+}
+
+resource "aws_ecr_repository_policy" "main" {
+  count      = local.permissions_create ? 1 : 0
+  repository = aws_ecr_repository.main.name
+  policy     = element(concat(data.aws_iam_policy_document.permissions[*].json, [""]), 0)
 }
--- aws/ecr/variables.tf
+++ aws/ecr/variables.tf
@@ -1,4 +1,15 @@
 locals {
+  permissions_read_write = merge(
+    length(var.push_roles) > 0 ? { AWS = var.push_roles } : {},
+    length(var.push_service_identifiers) > 0 ? { Service = var.push_service_identifiers } : {},
+  )
+  permissions_read_only = merge(
+    length(var.pull_roles) > 0 ? { AWS = var.pull_roles } : {},
+    length(var.pull_service_identifiers) > 0 ? { Service = var.pull_service_identifiers } : {},
+  )
+  permissions_create = length(local.permissions_read_write) > 0 || length(local.permissions_read_only) > 0 ? true : false
+
+  lifecycle_policy = var.lifecycle_policy == "default" ? var.cloudinfra_lifecycle : var.lifecycle_policy
   tags_base = {
     Billing     = var.billing
     CodeManaged = var.code_managed
@@ -19,13 +30,48 @@ variable "encryption_type" {
 }
 
 variable "image_tag_mutability" {
-  default = "MUTABLE"
+  description = "The tag mutability setting for the repository. Must be one of: MUTABLE or IMMUTABLE. Defaults to MUTABLE."
+  type        = string
+  default     = "MUTABLE"
 }
 
-variable "lifecycle_policy" { default = null }
+variable "kms_key" {
+  description = "ARN of the KMS to use when encryption type is KMS"
+  type        = string
+  default     = null
+}
+
+variable "lifecycle_policy" { default = "default" }
+variable "lifecycle_policy_enable" { default = true }
+
+variable "push_roles" {
+  description = "Role ARN's that have read/write access"
+  type        = list(string)
+  default     = []
+}
+
+variable "push_service_identifiers" {
+  description = "Service identifiers that have read/write access"
+  type        = list(string)
+  default     = []
+}
+
+variable "pull_roles" {
+  description = "Role ARN's that have read-only access"
+  type        = list(string)
+  default     = []
+}
+
+variable "pull_service_identifiers" {
+  description = "Service identifiers that have read-only access"
+  type        = list(string)
+  default     = []
+}
 
 variable "scan_on_push" {
-  default = true
+  description = "Enable image scanning on push"
+  type        = string
+  default     = true
 }
 
 # Tags
