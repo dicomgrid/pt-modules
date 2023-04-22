9411a714bb7f8cf876ead5b1bd1d07e93610c6de
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-21 21:50:35 -0500

    PLT-887: Add ownership controls to s3

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -4,8 +4,15 @@ resource "aws_s3_bucket" "main" {
   tags = local.tags
 }
 
-resource "aws_s3_bucket_acl" "main" {
+resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = var.object_ownership
+  }
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket_ownership_controls.main.id
   acl    = var.acl
 }
 
