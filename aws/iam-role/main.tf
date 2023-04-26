f2ff3f4c4494d5eed068b0b46d884eb583482761
cwilliams-intelerad <cody.williams@intelerad.com>
2023-04-25 23:45:49 -0500

    PLT-887: re-enable policies; add tags

--- aws/iam-role/main.tf
+++ aws/iam-role/main.tf
@@ -1,18 +1,19 @@
 resource "aws_iam_role" "main" {
-  name               = var.name
+  name               = var.tags.Name
   assume_role_policy = var.assume_role_policy
+  tags = var.tags
 }
 
-# Redundant to assume_role_polcy above
-# resource "aws_iam_role_policy" "main" {
-#   count  = var.policy == null ? 0 : 1
-#   name   = var.name
-#   role   = aws_iam_role.main.id
-#   policy = var.policy
-# }
+resource "aws_iam_role_policy" "main" {
+  count  = var.policy == null ? 0 : 1
+  name   = var.tags.Name
+  role   = aws_iam_role.main.id
+  policy = var.policy
+}
 
 resource "aws_iam_instance_profile" "main" {
   count = var.instance_profile ? 1 : 0
-  name = var.name
+  name = var.tags.Name
   role = aws_iam_role.main.id
+  tags = var.tags
 }
--- aws/iam-role/variables.tf
+++ aws/iam-role/variables.tf
@@ -1,3 +1,5 @@
-variable "name" {}
+# variable "name" {}
 variable "assume_role_policy" {}
 variable "instance_profile" { default = false }
+variable "policy" {}
+variable "tags" {}
