c73a5c46cf5cbdc6d5ddda59dd4baa47d0e20ed9
cwilliams-intelerad <81389845+cwilliams-intelerad@users.noreply.github.com>
2023-05-16 14:39:32 -0500

    PLT-1044: add features for user policy w/o group (#116)
    ## Submission type
    <!--- Issue Type -->
    - [] Bug fix/cleanup (non-breaking change which fixes known
    deficiencies)
    - [] New feature (non-breaking change which adds functionality)
    - [x] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    #Please verify there is a corresponding jira task for this commit.

--- aws/iam-user/main.tf
+++ aws/iam-user/main.tf
@@ -1,20 +1,25 @@
 resource "aws_iam_user" "main" {
   name          = var.name
   path          = var.path
-  force_destroy = var.service_account
-  tags = {
-    Compliance  = lookup(var.tags, "compliance", "NONE")
-    Email       = var.email
-    Environment = lookup(var.tags, "environment", "Master")
-    Name        = "iam_user:${var.name}"
-    Owner       = lookup(var.tags, "owner", "Platform")
-    Product     = lookup(var.tags, "product", "Platform Infrastructure")
-    User_Name   = "${var.first_name} ${var.last_name}"
-    User_Type   = var.user_type
-  }
+  force_destroy = var.service_account_map[var.service_account]
+  tags          = local.tags
+}
+
+resource "aws_iam_user_policy" "main" {
+  count  = var.inline_policy != null ? 1 : 0
+  name   = "${var.name}-user-policy"
+  user   = aws_iam_user.main.name
+  policy = var.inline_policy
+}
+
+resource "aws_iam_user_policy_attachment" "main" {
+  for_each = data.aws_iam_policy.aws
+  user   = aws_iam_user.main.name
+  policy_arn = each.value.arn
 }
 
 resource "aws_iam_user_group_membership" "main" {
+  count = var.groups != [] ? 1 : 0
   user   = aws_iam_user.main.name
   groups = var.groups
 }
--- aws/iam-user/vairables.tf
+++ aws/iam-user/vairables.tf
@@ -1,7 +1,8 @@
-variable "groups" {}
-variable "name" {}
+variable "aws_policies" { default = [] }
+variable "groups" { default = [] }
+variable "inline_policy" { default = null }
 variable "path" { default = null }
-variable "service_account" {}
+variable "service_account" { default = false }
 variable "service_account_map" {
   type = map(any)
   default = {
@@ -10,30 +11,32 @@ variable "service_account_map" {
   }
 }
 
-
 # Tags
 locals {
   default_tags = {
-    CodeManaged = var.code_managed
-    Compliance  = var.compliance
-    Email       = var.email
-    Environment = var.environment
-    Name        = "iam_user:${var.name}"
-    Owner       = var.owner
-    Product     = var.product
-    User_Name   = "${var.first_name} ${var.last_name}"
-    User_Type   = var.user_type
+    CodeManaged              = var.code_managed
+    Compliance               = var.compliance
+    Email                    = var.email
+    Name                     = "iam_user:${var.name}"
+    OneTime                  = var.onetime
+    Product                  = var.product
+    User_Name                = "${var.first_name} ${var.last_name}"
+    User_Type                = var.user_type
+    aws-migration-project-id = var.aws_project_id
+    map-migrated             = var.map_migrated
   }
   tags = merge(local.default_tags, var.tags)
 }
 variable "tags" {}
 
-variable "billing" { default = "platform" }
+variable "aws_project_id" { default = "null" }
+variable "code_managed" { default = "true" }
 variable "compliance" { default = "null" }
 variable "email" {}
-variable "environment" { default = "master" }
 variable "first_name" {}
 variable "last_name" {}
-variable "owner" { default = "platform" }
+variable "map_migrated" { default = "null" }
+variable "name" {}
+variable "onetime" { default = "null" }
 variable "product" { default = "iam" }
 variable "user_type" {}
