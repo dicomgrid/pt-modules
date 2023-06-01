e5633ff1e4a229bd6e6fbd9133b2c1b8fa984689
aknighton <andrew.knighton@intelerad.com>
2023-06-01 15:40:47 -0400

    PLT-1105: adding logic to get subnet ids

--- aws/ram/ram-transit-gw-attachment/main.tf
+++ aws/ram/ram-transit-gw-attachment/main.tf
@@ -2,7 +2,7 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
   provider = aws.second
 
-  subnet_ids         = var.subnet_ids
+  subnet_ids         = [for subnet_ids in local.az_subnets : subnet_ids[0]]
   transit_gateway_id = var.tgw_id
   vpc_id             = var.vpc_id
 
--- aws/ram/ram-transit-gw-attachment/variables.tf
+++ aws/ram/ram-transit-gw-attachment/variables.tf
@@ -1,11 +1,17 @@
+#creating a map of all subnets in an az per vpc
+locals {
+  az_subnets = {
+    for s in data.aws_subnet.main : s.availability_zone => s.id...
+  }
+}
 
 variable "tgw_id" {
   description = "The tgw id of the tgw that is to be shared"
 }
 
-variable "subnet_ids" {
-  description = "list of subnet ids to attach to tgw. need one for each az"
-}
+# variable "subnet_ids" {
+#   description = "list of subnet ids to attach to tgw. need one for each az"
+# }
 
 variable "vpc_id" {
   description = "The ID of the VPC where the subnets are located"
