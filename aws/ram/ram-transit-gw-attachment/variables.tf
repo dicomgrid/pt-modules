d4db7814de8988397372580ead376aa20ca3a217
aknighton <andrew.knighton@intelerad.com>
2023-06-01 15:42:01 -0400

    PLT-1105: clean up

--- aws/ram/ram-transit-gw-attachment/variables.tf
+++ aws/ram/ram-transit-gw-attachment/variables.tf
@@ -9,9 +9,6 @@ variable "tgw_id" {
   description = "The tgw id of the tgw that is to be shared"
 }
 
-# variable "subnet_ids" {
-#   description = "list of subnet ids to attach to tgw. need one for each az"
-# }
 
 variable "vpc_id" {
   description = "The ID of the VPC where the subnets are located"
--- aws/ram/ram-transit-gw/main.tf
+++ aws/ram/ram-transit-gw/main.tf
@@ -19,27 +19,3 @@ resource "aws_ram_principal_association" "main" {
   principal          = each.value
   resource_share_arn = aws_ram_resource_share.main.arn
 }
-
-
-# # Create the VPC attachment in the second account...
-# resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
-#   provider = aws.second
-
-#   depends_on = [
-#     aws_ram_principal_association.main,
-#     aws_ram_resource_association.main,
-#   ]
-
-#   subnet_ids         = [data.aws_subnets.main.id]
-#   transit_gateway_id = var.tgw_id
-#   vpc_id             = var.vpc_id
-
-# }
-
-# # ...and accept it in the first account.
-# resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "main" {
-#   provider = aws.first
-
-#   transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.main.id
-
-# }
