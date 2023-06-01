44ffa9da97fdb969ff5f0588e4c17c000106836f
austin.carter <austin.carter@intelerad.com>
2023-06-01 18:45:35 -0500

    moved variables out of main

--- aws/ram/ram-cidr-shares/main.tf
+++ aws/ram/ram-cidr-shares/main.tf
@@ -1,28 +1,4 @@
-variable "region" {
-  description = "The AWS region to create resources in"
-  type        = string
-  default     = "us-east-2"
-}
 
-variable "vpc_id" {
-  description = "The ID of the VPC where the subnets are located"
-  type        = string
-
-}
-
-variable "default_tags" {
-  description = "Default tags to be applied to all resources"
-  type        = map(string)
-  default = {
-    Environment = "Production"
-    Creator     = "Terraform"
-  }
-}
-
-variable "account_ids" {
-  description = "The AWS Account ID numbers of the accounts with which you are sharing your subnet"
-  type        = list(string)
-}
 
 provider "aws" {
   region = var.region
