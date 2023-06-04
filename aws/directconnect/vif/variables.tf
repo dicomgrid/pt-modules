b93b9816d37436fde2e32353ac6fb1e86b4008a1
acarter-ims <austin.carter@intelerad.com>
2023-06-04 05:10:34 +0000

    updating variables.tf

--- aws/directconnect/vif/variables.tf
+++ aws/directconnect/vif/variables.tf
@@ -1,21 +1,27 @@
 variable "connection_id" { 
    description = "The ID of the pending directconnect connection"
-   default = null
    }
-
 variable "route_type_identifier" {
    default = "CiscoSystemsInc-Nexus9KSeriesSwitches-NXOS93"
    }
 variable "confirm_dx_connection" {
     description = "Whether to confirm the directconnect connection"
     type = bool 
-    default = false
    }
    
+variable "dx_gateway_id" {
+    description = "The ID of the directconnect gateway"
+    default = null
+   }
+   
+variable "dx_transit_hosted_vif_address_family" {
+    description = "The address family of the directconnect hosted transit virtual interface"
+    default = "ipv4"
+   }
 variable "create_dx_hosted_transit_vif" {
     description = "Whether to create a directconnect gateway"
     type = bool
-    default = null
+    default = true
    }
    
 variable "create_dx_gateway" {
@@ -23,16 +29,6 @@ variable "create_dx_gateway" {
     type = bool 
     default = false
    }
-variable "dx_gateway_name" {
-      description = "The name of the directconnect gateway"
-      default = null 
-    }
-
-
-variable "dx_gateway_amazon_side_as" {
-      description = "The amazon side asn of the directconnect gateway"
-      default = null
-}
 variable "dx_transit_hosted_vif_name" {
       description = "The name of the directconnect hosted transit virtual interface"
       default = null
@@ -43,24 +39,13 @@ variable "dx_transit_hosted_vif_vlan_id" {
       default = null
 }
 
-variable "dx_gateway_id" {
-      description = "The ID of the directconnect gateway"
-      default = null
-}
+
+
 variable "dx_transit_hosted_vif_bgp_as" {
       description = "The BGP ASN of the directconnect hosted transit virtual interface"
       default = null
 }
 
-variable "dx_transit_hosted_vif_customer_address_family" {
-      description = "The customer address family of the directconnect hosted transit virtual interface"
-      default = "ipv4"
-}
-
-variable "dx_transit_hosted_vif_address_family" {
-      description = "The address family of the directconnect hosted transit virtual interface"
-      default = "ipv4"
-}
 variable "dx_transit_hosted_vif_customer_address" {
       description = "The customer address of the directconnect hosted transit virtual interface"
       default = null
