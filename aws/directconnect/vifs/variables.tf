5bb43df8ec6cb47c9597cd1489ead5edd387c7af
acarter-ims <austin.carter@intelerad.com>
2023-06-04 21:47:15 +0000

    changing modules due to bug https://github.com/hashicorp/terraform-provider-aws/issues/12336

--- aws/directconnect/vifs/variables.tf
+++ aws/directconnect/vifs/variables.tf
@@ -1,38 +1,39 @@
+variable "create_dx_gateway" {
+     description = "Whether to create a directconnect gateway"
+     type = bool
+     default = false
+}
 variable "connection_id" { 
    description = "The ID of the pending directconnect connection"
    }
-variable "owner_account_id" {
-   description = "The ID of the AWS account that owns the directconnect gateway"
-   default = null
-}
-variable "dx_transit_hosted_vif_address_family" {
+variable "dx_transit_vif_address_family" {
     description = "The address family of the directconnect hosted transit virtual interface"
     default = "ipv4"
    }
    
-variable "create_dx_hosted_transit_vif" {
+variable "create_dx_transit_vif" {
     description = "Whether to create a directconnect gateway"
     type = bool
    }
    
-variable "dx_transit_hosted_vif_name" {
+variable "dx_transit_vif_name" {
       description = "The name of the directconnect hosted transit virtual interface"
 }
 
-variable "dx_transit_hosted_vif_vlan_id" {
+variable "dx_transit_vif_vlan_id" {
       description = "The VLan ID of the directconnect hosted transit virtual interface"
 }
 
-variable "dx_transit_hosted_vif_bgp_as" {
+variable "dx_transit_vif_bgp_as" {
       description = "The BGP ASN of the directconnect hosted transit virtual interface"
 }
 
-variable "dx_transit_hosted_vif_customer_address" {
+variable "dx_transit_vif_customer_address" {
       description = "The customer address of the directconnect hosted transit virtual interface"
       default = null
     }
 
-variable "dx_transit_hosted_vif_amazon_address" {
+variable "dx_transit_vif_amazon_address" {
       description = "The amazon address of the directconnect hosted transit virtual interface"
       default = null
     }
@@ -41,3 +42,13 @@ variable "mtu_size" {
      description = "The MTU size of the directconnect hosted transit virtual interface"
      default = null
 }
+
+variable "dx_gateway_name" {
+     description = "The name of the directconnect gateway"
+     default = null
+}
+
+variable "amazon_side_asn" {
+    description = "The amazon side ASN of the directconnect gateway"
+    default = null
+    }
