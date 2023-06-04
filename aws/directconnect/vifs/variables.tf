4a86df2a853292044763e154b33aeac9e5ceaa5a
acarter-ims <austin.carter@intelerad.com>
2023-06-04 08:36:06 +0000

    sigh

--- aws/directconnect/vifs/variables.tf
+++ aws/directconnect/vifs/variables.tf
@@ -1,66 +1,47 @@
 variable "connection_id" { 
    description = "The ID of the pending directconnect connection"
    }
-   variable "dx_gateway_id" {
-    description = "The ID of the directconnect gateway"
-    default = null
-   }
 variable "route_type_identifier" {
    default = "CiscoSystemsInc-Nexus9KSeriesSwitches-NXOS93"
    }
-variable "confirm_dx_connection" {
-    description = "Whether to confirm the directconnect connection"
-    type = bool 
-    default = false
-   }
-variable "dx_gateway_amazon_side_as" {
-default = 0
-}
-variable "dx_gateway_name" {
-default = ""
-}
 
+variable "dx_gateway_id" {
+    description = "The ID of the directconnect gateway"
+   }
    
 variable "dx_transit_hosted_vif_address_family" {
     description = "The address family of the directconnect hosted transit virtual interface"
-    default = "ipv4"
+    default = ipv4
    }
 variable "create_dx_hosted_transit_vif" {
-    description = "Whether to create a transit vif"
-    type = bool
-    default = false
-   
-variable "create_dx_gateway" {
     description = "Whether to create a directconnect gateway"
-    type = bool 
-    default = false
+    type = bool
    }
+   
+
 variable "dx_transit_hosted_vif_name" {
       description = "The name of the directconnect hosted transit virtual interface"
-      default = ""
 }
 
 variable "dx_transit_hosted_vif_vlan_id" {
       description = "The VLan ID of the directconnect hosted transit virtual interface"
-      default = null
 }
-
-
 
 variable "dx_transit_hosted_vif_bgp_as" {
       description = "The BGP ASN of the directconnect hosted transit virtual interface"
-      default = null
 }
 
 variable "dx_transit_hosted_vif_customer_address" {
       description = "The customer address of the directconnect hosted transit virtual interface"
+      default = null
     }
 
 variable "dx_transit_hosted_vif_amazon_address" {
       description = "The amazon address of the directconnect hosted transit virtual interface"
-   
+      default = null
     }
 
 variable "mtu_size" {
      description = "The MTU size of the directconnect hosted transit virtual interface"
+     default = null
 }
