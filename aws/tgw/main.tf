d9c35723cb5c9a9ae6e382c0a4bda76ddcd7f542
cwilliams-intelerad <cody.williams@intelerad.com>
2023-06-05 11:19:25 -0500

    PLT-1105: add tgw and tgw peering

---
+++ aws/tgw/main.tf
@@ -0,0 +1,13 @@
+resource "aws_ec2_transit_gateway" "main" {
+
+  amazon_side_asn                 = var.amazon_side_asn
+  auto_accept_shared_attachments  = var.auto_accept_shared_attachments
+  default_route_table_association = var.default_route_table_association
+  default_route_table_propagation = var.default_route_table_propagation
+  description                     = var.description
+  dns_support                     = var.dns_support
+  multicast_support               = var.multicast_support
+  transit_gateway_cidr_blocks     = var.transit_gateway_cidr_blocks
+  vpn_ecmp_support                = var.vpn_ecmp_support
+  tags                            = local.tags
+}
---
+++ aws/tgw/peering-attachment/variables.tf
@@ -0,0 +1,25 @@
+variable "accepter_account_id" { default = null }
+variable "accepter_region" {}
+variable "accepter_transit_gateway_id" {}
+variable "accepter_transit_gateway" {}
+variable "requester_transit_gateway_id" {}
+variable "requester_transit_gateway" {}
+
+# Tags
+locals {
+  default_tags = {
+    CodeManaged              = var.code_managed
+    Compliance               = var.compliance
+    OneTime                  = var.onetime
+    aws-migration-project-id = var.aws_project_id
+    map-migrated             = var.map_migrated
+  }
+  tags = merge(local.default_tags, var.tags)
+}
+variable "tags" {}
+
+variable "aws_project_id" { default = "null" }
+variable "code_managed" { default = "true" }
+variable "compliance" { default = "null" }
+variable "map_migrated" { default = "null" }
+variable "onetime" { default = "null" }
