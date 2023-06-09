f373d6e6bda579b60904dea0b92a8b1c9c2d22a6
cwilliams-intelerad <81389845+cwilliams-intelerad@users.noreply.github.com>
2023-06-09 08:36:24 -0500

    PLT-1105: tgw, peering, vpc modules (#130)
    ## Submission type
    <!--- Issue Type -->
    - [] Bug fix/cleanup (non-breaking change which fixes known
    deficiencies)
    - [x] New feature (non-breaking change which adds functionality)
    - [] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    #Please verify there is a corresponding jira task for this commit.

--- aws/tgw/vpc-attachment/variables.tf
+++ aws/tgw/vpc-attachment/variables.tf
@@ -1,10 +1,14 @@
-variable "accepter_account_id" { default = null }
-variable "accepter_region" {}
-variable "accepter_transit_gateway_id" {}
-variable "accepter_transit_gateway" {}
-variable "requester_routes" { default = [] }
-variable "requester_transit_gateway_id" {}
-variable "requester_transit_gateway" {}
+# variable "accepter_account_id" { default = null }
+# variable "accepter_region" {}
+variable "subnets_tag_name" { 
+  default = "*"
+  description = "Addtional filter option for subnets to use with VPC attachment"
+}
+variable "tgw_id" {}
+# variable "accepter_transit_gateway" {}
+# variable "requester_routes" { default = [] }
+variable "vpc_id" {}
+# variable "requester_transit_gateway" {}
 
 # Tags
 locals {
@@ -17,8 +21,8 @@ locals {
   }
   tags = merge(local.default_tags, var.tags)
 }
-variable "tags" {}
 
+variable "tags" {}
 variable "aws_project_id" { default = "null" }
 variable "code_managed" { default = "true" }
 variable "compliance" { default = "null" }
