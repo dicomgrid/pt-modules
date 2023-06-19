a4cd3172be37c670a14df8f43dcd2253f49e9f82
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-06-19 14:18:23 -0500

    PLT-1129: Added generic resource group module

---
+++ aws/resource-group/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  type        = string
+  description = "Resource Group Name"
+  default     = ""
+}
+
+variable "description" {
+  type        = string
+  description = "Description of resource group"
+  default     = ""
+
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Resource Group Tags"
+  default = {
+    "Customer" = "",
+    "Group"    = "",
+    "Product"    = ""
+  }
+}
+
+variable "resource_query" {default = null}
+
