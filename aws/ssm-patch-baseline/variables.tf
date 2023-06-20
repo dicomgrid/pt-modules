55d0170e7c1e8003c789373b2fc02b834eb741c2
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-06-20 10:42:17 -0500

    PLT-1129: readded default base repo

--- aws/ssm-patch-baseline/variables.tf
+++ aws/ssm-patch-baseline/variables.tf
@@ -28,3 +28,37 @@ variable "compliance_levels" {
       },
   ]
 }
+
+variable "source_repos" {
+  type = list(object({
+    name = string
+    products = list(string)
+    configuration = string
+  }))
+  default = [
+    {
+      name = "base"
+      products = [
+        "CentOS7.0",
+        "CentOS7.1",
+        "CentOS7.2",
+        "CentOS7.3",
+        "CentOS7.4",
+        "CentOS7.5",
+        "CentOS7.6",
+        "CentOS7.7",
+        "CentOS7.8",
+        "CentOS7.9"
+        ]
+      configuration = <<EOF
+[base]
+name=CentOS-$releasever - Base
+#mirrorlist=http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra
+baseurl=http://mirror.centos.org/centos/$releasever/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+exclude=corosync pacemaker
+EOF
+    },
+  ]
+}
