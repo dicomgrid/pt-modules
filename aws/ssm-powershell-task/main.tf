77ab3f4fe6e3b8810a3981722903e0830e6e1a91
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-06-20 14:16:24 -0500

    PLT-1059: Added ssm-powershell-task module

---
+++ aws/ssm-powershell-task/main.tf
@@ -0,0 +1,25 @@
+resource "aws_ssm_maintenance_window_task" "ssm-powershell-task" {
+  name             = var.powershell_task_name
+  window_id        = var.powershell_task_window_id
+  task_type        = "RUN_COMMAND"
+  task_arn         = "AWS-RunPowerShellScript"
+  priority         = var.win_evac_priority
+  service_role_arn = var.service_role_arn
+  max_concurrency  = var.max_concurrency
+  max_errors       = var.max_errors
+  #TODO: Add cloudwatch event triggers
+  targets {
+    key    = "WindowTargetIds"
+    values = [var.powershell_task_window_target_id]
+  }
+  task_invocation_parameters {
+    run_command_parameters {
+      service_role_arn     = var.service_role_arn
+      timeout_seconds      = var.timeout_seconds
+      parameter {
+        name = "commands"
+        values = ["${file("${path.module}/../ssm-tools/scripts/${var.powershell_task}")}"]
+      }
+    }
+  }
+}
