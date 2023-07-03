88dbc3fd88aaa228c86af80194e5b4d8b2d83b8a
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-07-03 18:26:28 -0500

    PLT-1232: Added safety and logging to pacs scripts

--- aws/ssm-tools/scripts/start-pacs.sh
+++ aws/ssm-tools/scripts/start-pacs.sh
@@ -2,10 +2,8 @@
 set -e
 
 function startPacs() {
-    if [ -e $(ls /etc/rc.d/init.d/PACS) ]; 
+    if [[ -e $(ls /etc/rc.d/init.d/PACS) ]]
     then
-        sudo su - admin
-
         echo "Attempting to start sybase..."
         /opt/intelerad/bin/padmin sudo /etc/init.d/sybase -v start > /dev/null 2>&1
         echo "Attempting to start postgres-intelerad..."
@@ -17,35 +15,67 @@ function startPacs() {
         echo "Attempting to start IntelePACS services..."
         /opt/intelerad/bin/padmin PACS start
         
-        echo "Reporting Post start status"
-        sudo service pacemaker status
+        echo -e "\e"
+        echo -e "\e"
+        echo "Reporting Post service start status"
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "sybase status"
         sudo service sybase status
-        sudo service postgres-intelerad status
-        sudo service apache2 status
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "postgres-intelerad status"
+        sudo service postgres-intelerad status | head -n 7
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "apache2 status"
+        sudo service apache2 status | head -n 7
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "Tomcat status"
         /opt/intelerad/bin/padmin controlTomcat -t list
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "PACS status"
         /opt/intelerad/bin/padmin PACS status
-
-        if [ -e $(ls /etc/rc.d/init.d/corosync) ];
+        echo -e "\e"
+        echo -e "\e"
+        if [[ -e $(ls /etc/rc.d/init.d/corosync > /dev/null 2>&1) ]]
         then
             echo "Attempting to start corosync..."
             sudo systemctl start corosync > /dev/null 2>&1
-            sudo service corosync status
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "corosync status"
+            sudo service corosync status | head -n 7
+            
             echo "Attempting to start pacemaker..."
             sudo systemctl start pacemaker > /dev/null 2>&1
-            sudo systemctl status pacemaker
-        ;
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "pacemaker status"
+            sudo service pacemaker status | head -n 7
         else
             echo "No clustering services to start, proceeding..."
-        ;
-        exit 0
-    ;
-    else 
+        fi
+        echo "start service tasks completed."
+    else
         echo "PACS services not found, exiting..."
-        exit 0
-    ; 
     fi
 }
 
-startPacs || exit 1
+function handleErr() {
+    exitCode=$?
+    lastCommand=${BASH_COMMAND}
+    echo "stderr occurred!"
+    echo "last command: $lastCommand"
+    echo -e "exit code: $exitCode"
+    exit $exitCode
+}
+trap handleErr ERR
 
-
+function main() {
+    export -f startPacs
+    su admin -c "bash -c startPacs"
+}
+main
