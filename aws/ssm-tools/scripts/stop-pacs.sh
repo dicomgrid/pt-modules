33b5ae9eb16e0648ad8d2292c39bf136be5dc350
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-07-03 18:26:28 -0500

    PLT-1232: Added safety and logging to pacs scripts

--- aws/ssm-tools/scripts/stop-pacs.sh
+++ aws/ssm-tools/scripts/stop-pacs.sh
@@ -2,22 +2,26 @@
 set -e
 
 function stopPacs() {
-    if [ -e $(ls /etc/rc.d/init.d/PACS) ]; 
+    if [[ -e $(ls /etc/rc.d/init.d/PACS) ]] 
     then
-        sudo su - admin
-        if [ -e $(ls /etc/rc.d/init.d/corosync) ];
+        if [[ -e $(ls /etc/rc.d/init.d/corosync > /dev/null 2>&1) ]]
         then
             echo "Attempting to stop corosync..."
             sudo systemctl stop corosync > /dev/null 2>&1
-            sudo service corosync status
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "corosync status"
+            sudo service corosync status | head -n 7
+
             echo "Attempting to stop pacemaker..."
             sudo systemctl stop pacemaker > /dev/null 2>&1
-            sudo systemctl status pacemaker
-        ;
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "pacemaker status"
+            sudo service pacemaker status | head -n 7
         else
             echo "No clustering services to stop, proceeding..."
-        ;
-        
+        fi
         echo "Attempting to stop IntelePACS services..."
         /opt/intelerad/bin/padmin PACS stop > /dev/null 2>&1
         echo "Attempting to stop Tomcat..."
@@ -29,13 +33,47 @@ function stopPacs() {
         echo "Attempting to stop Sybase service..."
         /opt/intelerad/bin/padmin sudo /etc/init.d/sybase -v stop > /dev/null 2>&1
         
-        exit 0
-        ;
+        echo -e "\e"
+        echo -e "\e"
+        echo "Reporting Post service stop status"
+        echo -e "sybase status"
+        sudo service sybase status
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "postgres-intelerad status"
+        sudo service postgres-intelerad status | head -n 7
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "apache2 status"
+        sudo service apache2 status | head -n 7
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "Tomcat status"
+        /opt/intelerad/bin/padmin controlTomcat -t list
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "PACS status"
+        /opt/intelerad/bin/padmin PACS status
+        echo -e "\e"
+        echo -e "\e"
+        echo "stop service tasks completed."
     else
         echo "PACS services not found, exiting..."
-        exit 0
-    ;
     fi
 }
 
-stopPacs || exit 1
+function handleErr() {
+    exitCode=$?
+    lastCommand=${BASH_COMMAND}
+    echo "stderr occurred!"
+    echo "last command: $lastCommand"
+    echo -e "exit code: $exitCode"
+    exit $exitCode
+}
+trap handleErr ERR
+
+function main() {
+    export -f stopPacs
+    su admin -c "bash -c stopPacs"
+}
+main
