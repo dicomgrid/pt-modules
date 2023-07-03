7351b1d8789ee45f49cc5424f828469e3e715f9f
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-07-03 15:19:59 -0500

    PLT-1232: Added conditional reboot

--- aws/ssm-tools/scripts/patch-pacs.sh
+++ aws/ssm-tools/scripts/patch-pacs.sh
@@ -3,3 +3,11 @@ export REPOS="--disablerepo=* --enablerepo=centos7-x86_64 --enablerepo=epel --en
 export SKIP="--exclude=pacemaker* --exclude=corosync*"
 sudo yum --disableplugin=rhnplugin  update ca-certificates -y
 sudo yum ${REPOS} ${SKIP} update -y
+
+needs-restarting -r
+if [ $? -eq 1 ]
+then
+        exit 194
+else
+        exit 0
+fi
