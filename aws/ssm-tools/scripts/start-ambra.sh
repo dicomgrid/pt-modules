921c1ca21796c0a3c565d8da41b9b4a78e38486e
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-07-06 14:12:23 -0500

    PLT-557: added check-ambra.sh

--- aws/ssm-tools/scripts/start-ambra.sh
+++ aws/ssm-tools/scripts/start-ambra.sh
@@ -3,94 +3,203 @@ set -e
 
 function startGrid() {
     #dga
-    if [[ -e $(ls /etc/systemd/system/dga.service) ]] 
+    dga_status=$(systemctl is-enabled dga)
+    dga_state=$(systemctl is-active dga)
+    if [[$dga_status == "enabled" ]] && [[ $dga_state != "active" ]] 
     then
-            echo "Attempting to start grid..."
-            sudo systemctl enable dga
-            sudo systemctl start dga
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "dga status"
-            sudo systemctl status dga | head -n 7
+        sleep 5
+        echo "dga not active, attempting to start..."
+        sudo systemctl start dga
+        dga_check1=$(systemctl is-active dga)
+        if [[ $dga_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start dga
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "dga status"
+                sudo systemctl status dga | head -n 7
+        fi
     fi
 
     #grid
-    if [[ -e $(ls /etc/systemd/system/grid.service) ]] 
+    grid_status=$(systemctl is-enabled grid)
+    grid_state=$(systemctl is-active grid)
+    if [[$grid_status == "enabled" ]] && [[ $grid_state != "active" ]] 
     then
-            echo "Attempting to start grid..."
-            sudo systemctl enable grid
-            sudo systemctl start grid
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "grid status"
-            sudo systemctl status grid | head -n 7
+        sleep 5
+        echo "grid not active, attempting to start..."
+        sudo systemctl start grid
+        grid_check1=$(systemctl is-active grid)
+        if [[ $grid_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start grid
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "grid status"
+                sudo systemctl status grid | head -n 7
+        fi
     fi
     
     #haproxy
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/haproxy.service) ]] 
+    haproxy_status=$(systemctl is-enabled haproxy)
+    haproxy_state=$(systemctl is-active haproxy)
+    if [[$haproxy_status == "enabled" ]] && [[ $haproxy_state != "active" ]] 
     then
-            echo "Attempting to start haproxy..."
-            sudo systemctl enable haproxy
-            sudo systemctl start haproxy
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "haproxy status"
-            sudo systemctl status haproxy | head -n 7
+        sleep 5
+        echo "haproxy not active, attempting to start..."
+        sudo systemctl start haproxy
+        haproxy_check1=$(systemctl is-active haproxy)
+        if [[ $haproxy_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start haproxy
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "haproxy status"
+                sudo systemctl status haproxy | head -n 7
+        fi
     fi
 
     #openresty
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/openresty.service) ]] 
+    openresty_status=$(systemctl is-enabled openresty)
+    openresty_state=$(systemctl is-active openresty)
+    if [[$openresty_status == "enabled" ]] && [[ $openresty_state != "active" ]] 
     then
-            echo "Attempting to start openresty..."
-            sudo systemctl start openresty
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "openresty status"
-            sudo systemctl status openresty
+        sleep 5
+        echo "openresty not active, attempting to start..."
+        sudo systemctl start openresty
+        openresty_check1=$(systemctl is-active openresty)
+        if [[ $openresty_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start openresty
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "openresty status"
+                sudo systemctl status openresty | head -n 7
+        fi
     fi
 
-    #redis
-    if [[ -e $(ls /etc/systemd/system/v3s-redis.service) ]] 
+    #transcoding*
+    transcoding_status=$(systemctl is-enabled transcoding*)
+    transcoding_state=$(systemctl is-active transcoding*)
+    if [[$transcoding_status == *"enabled"* ]] && [[ $transcoding_state != *"active"* ]] 
     then
-            echo "Attempting to start v3s-redis..."
-            sudo systemctl start v3s-redis
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "v3s-redis status"
-            sudo systemctl status v3s-redis
+        sleep 5
+        echo "transcoding not active, attempting to start..."
+        sudo systemctl start transcoding*
+        transcoding_check1=$(systemctl is-active transcoding*)
+        if [[ $transcoding_check1 != *"active"* ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start transcoding*
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "transcoding status"
+                sudo systemctl status transcoding* | head -n 7
+        fi
     fi
 
-    #transcoding
-    if [[ -e $(ls /etc/systemd/system/transcoding*) ]] 
+    #v3s-apache
+    v3s-apache_status=$(systemctl is-enabled v3s-apache)
+    v3s-apache_state=$(systemctl is-active v3s-apache)
+    if [[$v3s-apache_status == "enabled" ]] && [[ $v3s-apache_state != "active" ]] 
     then
-            echo "Attempting to start transcoding services..."
-            sudo systemctl start transcoding*
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "transcoding status"
-            sudo systemctl status transcoding*
+        sleep 5
+        echo "v3s-apache not active, attempting to start..."
+        sudo systemctl start v3s-apache
+        v3s-apache_check1=$(systemctl is-active v3s-apache)
+        if [[ $v3s-apache_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start v3s-apache
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "v3s-apache status"
+                sudo systemctl status v3s-apache | head -n 7
+        fi
     fi
 
-    #v3s
-    if [[ -e $(ls /home/v3s/bin/ctl) ]] 
+    #v3s-cron
+    v3s-cron_status=$(systemctl is-enabled v3s-cron)
+    v3s-cron_state=$(systemctl is-active v3s-cron)
+    if [[$v3s-cron_status == "enabled" ]] && [[ $v3s-cron_state != "active" ]] 
     then
-            echo "Attempting to start grid services..."
-            su v3s -c "bash -c /home/v3s/bin/ctl start_all"
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "ctl status"
-            su v3s -c "bash -c /home/v3s/bin/ctl status"
+        sleep 5
+        echo "v3s-cron not active, attempting to start..."
+        sudo systemctl start v3s-cron
+        v3s-cron_check1=$(systemctl is-active v3s-cron)
+        if [[ $v3s-cron_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start v3s-cron
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "v3s-cron status"
+                sudo systemctl status v3s-cron | head -n 7
+        fi
+    fi
+
+    #v3s-postgres
+    v3s-postgres_status=$(systemctl is-enabled v3s-postgres)
+    v3s-postgres_state=$(systemctl is-active v3s-postgres)
+    if [[$v3s-postgres_status == "enabled" ]] && [[ $v3s-postgres_state != "active" ]] 
+    then
+        sleep 5
+        echo "v3s-postgres not active, attempting to start..."
+        sudo systemctl start v3s-postgres
+        v3s-postgres_check1=$(systemctl is-active v3s-postgres)
+        if [[ $v3s-postgres_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start v3s-postgres
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "v3s-postgres status"
+                sudo systemctl status v3s-postgres | head -n 7
+        fi
+    fi
+
+    #v3s-redis
+    v3s-redis_status=$(systemctl is-enabled v3s-redis)
+    v3s-redis_state=$(systemctl is-active v3s-redis)
+    if [[$v3s-redis_status == "enabled" ]] && [[ $v3s-redis_state != "active" ]] 
+    then
+        sleep 5
+        echo "v3s-redis not active, attempting to start..."
+        sudo systemctl start v3s-redis
+        v3s-redis_check1=$(systemctl is-active v3s-redis)
+        if [[ $v3s-redis_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start v3s-redis
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "v3s-redis status"
+                sudo systemctl status v3s-redis | head -n 7
+        fi
     fi
 
     #watchdog-services
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/watchdog-services.service) ]] 
+    watchdog_status=$(systemctl is-enabled watchdog)
+    watchdog_state=$(systemctl is-active watchdog)
+    if [[$watchdog_status == "enabled" ]] && [[ $watchdog_state != "active" ]] 
     then
-            echo "Attempting to start watchdog services..."
-            sudo systemctl start watchdog-services
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "watchdog status"
-            sudo systemctl status watchdog-services
+        sleep 5
+        echo "watchdog not active, attempting to start..."
+        sudo systemctl start watchdog
+        watchdog_check1=$(systemctl is-active watchdog)
+        if [[ $watchdog_check1 != "active" ]] 
+                sleep 5
+                echo "failed to start 1 time, attempting to start again..."
+                sudo systemctl start watchdog
+                echo -e "\e"
+                echo -e "\e"
+                echo -e "watchdog status"
+                sudo systemctl status watchdog | head -n 7
+        fi
     fi
 }
 
--- aws/ssm-tools/scripts/stop-ambra.sh
+++ aws/ssm-tools/scripts/stop-ambra.sh
@@ -3,58 +3,115 @@ set -e
 
 function stopGrid() {
     #dga
-    if [[ -e $(ls /etc/systemd/system/dga.service) ]] 
+    dga_status=$(systemctl is-enabled dga)
+    dga_state=$(systemctl is-active dga)
+    if [[$dga_status == "enabled" ]] && [[ $dga_state != "active" ]]
     then
-            echo "Attempting to stop grid..."
-            sudo systemctl enable dga
-            sudo systemctl stop dga
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "dga status"
-            sudo systemctl status dga | head -n 7
+        echo "Attempting to stop dga..."
+        sudo systemctl stop dga
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "dga status"
+        sudo systemctl status dga | head -n 7
     fi
 
     #grid
-    if [[ -e $(ls /etc/systemd/system/grid.service) ]] 
+    grid_status=$(systemctl is-enabled grid)
+    grid_state=$(systemctl is-active grid)
+    if [[$grid_status == "enabled" ]] && [[ $grid_state != "active" ]] 
     then
-            echo "Attempting to stop grid..."
-            sudo systemctl enable grid
-            sudo systemctl stop grid
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "grid status"
-            sudo systemctl status grid | head -n 7
+        echo "Attempting to stop grid..."
+        sudo systemctl stop grid
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "grid status"
+        sudo systemctl status grid | head -n 7
     fi
     
     #haproxy
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/haproxy.service) ]] 
+    haproxy_status=$(systemctl is-enabled haproxy)
+    haproxy_state=$(systemctl is-active haproxy)
+    if [[$haproxy_status == "enabled" ]] && [[ $haproxy_state != "active" ]] 
     then
-            echo "Attempting to stop haproxy..."
-            sudo systemctl enable haproxy
-            sudo systemctl stop haproxy
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "haproxy status"
-            sudo systemctl status haproxy | head -n 7
+        echo "Attempting to stop haproxy..."
+        sudo systemctl stop haproxy
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "haproxy status"
+        sudo systemctl status haproxy | head -n 7
     fi
 
     #openresty
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/openresty.service) ]] 
+    openresty_status=$(systemctl is-enabled openresty)
+    openresty_state=$(systemctl is-active openresty)
+    if [[$openresty_status == "enabled" ]] && [[ $openresty_state != "active" ]] 
     then
-            echo "Attempting to stop openresty..."
-            sudo systemctl enable openresty
-            sudo systemctl stop openresty
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "openresty status"
-            sudo systemctl status openresty
+        echo "Attempting to stop openresty..."
+        sudo systemctl stop openresty
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "openresty status"
+        sudo systemctl status openresty
     fi
 
-    #redis
-    if [[ -e $(ls /etc/systemd/system/v3s-redis.service) ]] 
+    #transcoding*
+    transcoding_status=$(systemctl is-enabled transcoding*)
+    transcoding_state=$(systemctl is-active transcoding*)
+    if [[$transcoding_status == *"enabled"* ]] && [[ $transcoding_state != *"active"* ]] 
     then
-            echo "Attempting to stop v3s-redis..."
-            sudo systemctl enable v3s-redis
+        echo "Attempting to stop transcoding services..."
+        sudo systemctl stop transcoding*
+        echo -e "\e"
+        echo -e "\e"
+        echo -e "transcoding status"
+        sudo systemctl status transcoding*
+    fi
+
+    #v3s-apache
+    v3s-apache_status=$(systemctl is-enabled v3s-apache)
+    v3s-apache_state=$(systemctl is-active v3s-apache)
+    if [[$v3s-apache_status == "enabled" ]] && [[ $v3s-apache_state != "active" ]] 
+    then
+            echo "Attempting to stop v3s-apache services..."
+            sudo systemctl stop v3s-apache
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "v3s-apache status"
+            sudo systemctl status v3s-apache
+    fi
+
+    #v3s-cron
+    v3s-cron_status=$(systemctl is-enabled v3s-cron)
+    v3s-cron_state=$(systemctl is-active v3s-cron)
+    if [[$v3s-cron_status == "enabled" ]] && [[ $v3s-cron_state != "active" ]] 
+    then
+            echo "Attempting to stop v3s-cron services..."
+            sudo systemctl stop v3s-cron
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "v3s-cron status"
+            sudo systemctl status v3s-cron
+    fi
+
+    #v3s-postgres
+    v3s-postgres_status=$(systemctl is-enabled v3s-postgres)
+    v3s-postgres_state=$(systemctl is-active v3s-postgres)
+    if [[$v3s-postgres_status == "enabled" ]] && [[ $v3s-postgres_state != "active" ]] 
+    then
+            echo "Attempting to stop v3s-postgres services..."
+            sudo systemctl stop v3s-postgres
+            echo -e "\e"
+            echo -e "\e"
+            echo -e "v3s-postgres status"
+            sudo systemctl status v3s-postgres
+    fi
+
+    #v3s-redis
+    v3s-redis_status=$(systemctl is-enabled v3s-redis)
+    v3s-redis_state=$(systemctl is-active v3s-redis)
+    if [[$v3s-redis_status == "enabled" ]] && [[ $v3s-redis_state != "active" ]] 
+    then
+            echo "Attempting to stop v3s-redis services..."
             sudo systemctl stop v3s-redis
             echo -e "\e"
             echo -e "\e"
@@ -62,34 +119,12 @@ function stopGrid() {
             sudo systemctl status v3s-redis
     fi
 
-    #transcoding
-    if [[ -e $(ls /etc/systemd/system/transcoding*) ]] 
-    then
-            echo "Attempting to stop transcoding services..."
-            sudo systemctl enable transcoding*
-            sudo systemctl stop transcoding*
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "transcoding status"
-            sudo systemctl status transcoding*
-    fi
-
-    #v3s
-    if [[ -e $(ls /home/v3s/bin/ctl) ]] 
-    then
-            echo "Attempting to stop grid services..."
-            su v3s -c "bash -c /home/v3s/bin/ctl stop_all"
-            echo -e "\e"
-            echo -e "\e"
-            echo -e "ctl status"
-            su v3s -c "bash -c /home/v3s/bin/ctl status"
-    fi
-
     #watchdog-services
-    if [[ -e $(ls /etc/systemd/system/multi-user.target.wants/watchdog-services.service) ]] 
+    watchdog_status=$(systemctl is-enabled watchdog)
+    watchdog_state=$(systemctl is-active watchdog)
+    if [[$watchdog_status == "enabled" ]] && [[ $watchdog_state != "active" ]] 
     then
             echo "Attempting to stop watchdog services..."
-            sudo systemctl enable watchdog-services
             sudo systemctl stop watchdog-services
             echo -e "\e"
             echo -e "\e"
