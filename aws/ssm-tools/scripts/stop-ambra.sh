9381e37e3923899b4b11bc506c4cbd50835fdbea
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-07-07 18:54:36 -0500

    PLT-558: smoketested ambra scripts

--- aws/ssm-tools/scripts/stop-ambra.sh
+++ aws/ssm-tools/scripts/stop-ambra.sh
@@ -1,136 +1,193 @@
 #! /bin/bash
-set -e
-
-function stopGrid() {
-    #dga
-    dga_status=$(systemctl is-enabled dga)
-    dga_state=$(systemctl is-active dga)
-    if [[$dga_status == "enabled" ]] && [[ $dga_state != "active" ]]
+function stopDga() {
+    if [[ -f "/etc/systemd/system/dga.service" ]]
     then
-        echo "Attempting to stop dga..."
-        sudo systemctl stop dga
-        echo -e "\e"
+        dga_status=$(systemctl is-enabled dga)
+        dga_state=$(systemctl is-active dga)
+        if [[ $dga_status == "enabled" ]] && [[ $dga_state == "active" ]]
+        then
+            echo "Attempting to stop dga..."
+            sudo systemctl stop dga
+        fi
         echo -e "\e"
         echo -e "dga status"
-        sudo systemctl status dga | head -n 7
-    fi
-
-    #grid
-    grid_status=$(systemctl is-enabled grid)
-    grid_state=$(systemctl is-active grid)
-    if [[$grid_status == "enabled" ]] && [[ $grid_state != "active" ]] 
-    then
-        echo "Attempting to stop grid..."
-        sudo systemctl stop grid
+        sudo systemctl status dga
         echo -e "\e"
+    else
+        echo "dga does not exist"
+    fi
+}
+
+function stopGrid() {
+    if [[ -f "/etc/systemd/system/grid.service" ]]
+    then
+        grid_status=$(systemctl is-enabled grid)
+        grid_state=$(systemctl is-active grid)
+        if [[ $grid_status == "enabled" ]] && [[ $grid_state == "active" ]] 
+        then
+            echo "Attempting to stop grid..."
+            sudo systemctl stop grid
+        fi
         echo -e "\e"
         echo -e "grid status"
-        sudo systemctl status grid | head -n 7
-    fi
-    
-    #haproxy
-    haproxy_status=$(systemctl is-enabled haproxy)
-    haproxy_state=$(systemctl is-active haproxy)
-    if [[$haproxy_status == "enabled" ]] && [[ $haproxy_state != "active" ]] 
-    then
-        echo "Attempting to stop haproxy..."
-        sudo systemctl stop haproxy
+        sudo systemctl status grid
         echo -e "\e"
+    else
+        echo "grid does not exist"
+    fi   
+}
+    
+function stopHaproxy() {
+    if [[ -f /etc/systemd/system/multi-user.target.wants/haproxy.service ]]
+    then
+        haproxy_status=$(systemctl is-enabled haproxy)
+        haproxy_state=$(systemctl is-active haproxy)
+        if [[ $haproxy_status == "enabled" ]] && [[ $haproxy_state == "active" ]] 
+        then
+            echo "Attempting to stop haproxy..."
+            sudo systemctl stop haproxy
+        fi
         echo -e "\e"
         echo -e "haproxy status"
-        sudo systemctl status haproxy | head -n 7
-    fi
-
-    #openresty
-    openresty_status=$(systemctl is-enabled openresty)
-    openresty_state=$(systemctl is-active openresty)
-    if [[$openresty_status == "enabled" ]] && [[ $openresty_state != "active" ]] 
-    then
-        echo "Attempting to stop openresty..."
-        sudo systemctl stop openresty
+        sudo systemctl status haproxy
         echo -e "\e"
+    else
+        echo "haproxy does not exist"
+    fi 
+}
+
+function stopOpenresty() {
+    if [[ -f /etc/systemd/system/multi-user.target.wants/openresty.service ]]
+    then
+        openresty_status=$(systemctl is-enabled openresty)
+        openresty_state=$(systemctl is-active openresty)
+        if [[ $openresty_status == "enabled" ]] && [[ $openresty_state == "active" ]] 
+        then
+            echo "Attempting to stop openresty..."
+            sudo systemctl stop openresty
+        fi
         echo -e "\e"
         echo -e "openresty status"
-        sudo systemctl status openresty | head -n 7
-    fi
-
-    #transcoding*
-    transcoding_status=$(systemctl is-enabled transcoding*)
-    transcoding_state=$(systemctl is-active transcoding*)
-    if [[$transcoding_status == *"enabled"* ]] && [[ $transcoding_state != *"active"* ]] 
-    then
-        echo "Attempting to stop transcoding services..."
-        sudo systemctl stop transcoding*
+        sudo systemctl status openresty
         echo -e "\e"
+    else
+        echo "openresty does not exist"
+    fi 
+}
+
+
+function stopTranscoding() {
+    if [[ -f /etc/systemd/system/transcoding* ]]
+    then
+        transcoding_status=$(systemctl is-enabled transcoding*)
+        transcoding_state=$(systemctl is-active transcoding*)
+        if [[ $transcoding_status == *"enabled"* ]] && [[ $transcoding_state == *"active"* ]] 
+        then
+            echo "Attempting to stop transcoding services..."
+            sudo systemctl stop transcoding*
+            fi
         echo -e "\e"
         echo -e "transcoding status"
-        sudo systemctl status transcoding* | head -n 7
-    fi
-
-    #v3s-apache
-    v3s-apache_status=$(systemctl is-enabled v3s-apache)
-    v3s-apache_state=$(systemctl is-active v3s-apache)
-    if [[$v3s-apache_status == "enabled" ]] && [[ $v3s-apache_state != "active" ]] 
-    then
-        echo "Attempting to stop v3s-apache services..."
-        sudo systemctl stop v3s-apache
+        sudo systemctl status transcoding*
         echo -e "\e"
+    else
+        echo "transcoding does not exist"
+    fi 
+}
+
+function stopV3sApache() {
+    if [[ -f /etc/systemd/system/v3s-apache.service ]]
+    then
+        v3s-apache_status=$(systemctl is-enabled v3s-apache)
+        v3s-apache_state=$(systemctl is-active v3s-apache)
+        if [[ $v3s-apache_status == "enabled" ]] && [[ $v3s-apache_state == "active" ]] 
+        then
+            echo "Attempting to stop v3s-apache services..."
+            sudo systemctl stop v3s-apache
+        fi
         echo -e "\e"
         echo -e "v3s-apache status"
-        sudo systemctl status v3s-apache | head -n 7
-    fi
-
-    #v3s-cron
-    v3s-cron_status=$(systemctl is-enabled v3s-cron)
-    v3s-cron_state=$(systemctl is-active v3s-cron)
-    if [[$v3s-cron_status == "enabled" ]] && [[ $v3s-cron_state != "active" ]] 
-    then
-        echo "Attempting to stop v3s-cron services..."
-        sudo systemctl stop v3s-cron
+        sudo systemctl status v3s-apache
         echo -e "\e"
+    else
+        echo "v3s-apache does not exist"
+    fi 
+}
+
+function stopV3sCron() {
+    if [[ -f /etc/systemd/system/v3s-cron.service ]]
+    then
+        v3s-cron_status=$(systemctl is-enabled v3s-cron)
+        v3s-cron_state=$(systemctl is-active v3s-cron)
+        if [[ $v3s-cron_status == "enabled" ]] && [[ $v3s-cron_state == "active" ]] 
+        then
+            echo "Attempting to stop v3s-cron services..."
+            sudo systemctl stop v3s-cron
+        fi
         echo -e "\e"
         echo -e "v3s-cron status"
-        sudo systemctl status v3s-cron | head -n 7
-    fi
-
-    #v3s-postgres
-    v3s-postgres_status=$(systemctl is-enabled v3s-postgres)
-    v3s-postgres_state=$(systemctl is-active v3s-postgres)
-    if [[$v3s-postgres_status == "enabled" ]] && [[ $v3s-postgres_state != "active" ]] 
-    then
-        echo "Attempting to stop v3s-postgres services..."
-        sudo systemctl stop v3s-postgres
+        sudo systemctl status v3s-cron
         echo -e "\e"
+    else
+        echo "v3s-cron does not exist"
+    fi
+}
+
+function stopV3sPostgres() {
+    if [[ -f /etc/systemd/system/v3s-postgres.service ]]
+    then
+        v3s-postgres_status=$(systemctl is-enabled v3s-postgres)
+        v3s-postgres_state=$(systemctl is-active v3s-postgres)
+        if [[ $v3s-postgres_status == "enabled" ]] && [[ $v3s-postgres_state == "active" ]] 
+        then
+            echo "Attempting to stop v3s-postgres services..."
+            sudo systemctl stop v3s-postgres
+            fi
         echo -e "\e"
         echo -e "v3s-postgres status"
-        sudo systemctl status v3s-postgres | head -n 7
+        sudo systemctl status v3s-postgres
+        echo -e "\e"
+    else
+        echo "v3s-postgres does not exist"
     fi
+}
 
-    #v3s-redis
-    v3s-redis_status=$(systemctl is-enabled v3s-redis)
-    v3s-redis_state=$(systemctl is-active v3s-redis)
-    if [[$v3s-redis_status == "enabled" ]] && [[ $v3s-redis_state != "active" ]] 
+function stopV3sRedis() {
+    if [[ -f /etc/systemd/system/v3s-redis.service ]]
     then
-        echo "Attempting to stop v3s-redis services..."
-        sudo systemctl stop v3s-redis
-        echo -e "\e"
-        echo -e "\e"
-        echo -e "v3s-redis status"
-        sudo systemctl status v3s-redis | head -n 7
+        v3s-redis_status=$(systemctl is-enabled v3s-redis)
+        v3s-redis_state=$(systemctl is-active v3s-redis)
+        if [[ $v3s-redis_status == "enabled" ]] && [[ $v3s-redis_state == "active" ]] 
+        then
+            echo "Attempting to stop v3s-redis services..."
+            sudo systemctl stop v3s-redis
+        fi
+            echo -e "\e"
+            echo -e "v3s-redis status"
+            sudo systemctl status v3s-redis
+            echo -e "\e"
+    else
+        echo "v3s-redis does not exist"
     fi
+}
 
-    #watchdog-services
-    watchdog_status=$(systemctl is-enabled watchdog)
-    watchdog_state=$(systemctl is-active watchdog)
-    if [[$watchdog_status == "enabled" ]] && [[ $watchdog_state != "active" ]] 
+function stopWatchdog() {
+    if [[ -f /etc/systemd/system/multi-user.target.wants/watchdog-services.service ]]
     then
-        echo "Attempting to stop watchdog services..."
-        sudo systemctl stop watchdog-services
-        echo -e "\e"
-        echo -e "\e"
-        echo -e "watchdog status"
-        sudo systemctl status watchdog-services | head -n 7
-    fi 
+        watchdog_status=$(systemctl is-enabled watchdog)
+        watchdog_state=$(systemctl is-active watchdog)
+        if [[ $watchdog_status == "enabled" ]] && [[ $watchdog_state == "active" ]] 
+        then
+            echo "Attempting to stop watchdog services..."
+            sudo systemctl stop watchdog-services
+        fi
+            echo -e "\e"
+            echo -e "watchdog status"
+            sudo systemctl status watchdog
+            echo -e "\e"
+    else
+        echo "watchdog does not exist"
+    fi
 }
 
 function handleErr() {
@@ -143,4 +200,16 @@ function handleErr() {
 }
 trap handleErr ERR
 
-stopGrid
+function main() {
+    stopDga
+    stopGrid
+    stopHaproxy
+    stopOpenresty
+    stopTranscoding
+    stopV3sApache
+    stopV3sCron
+    stopV3sPostgres
+    stopV3sRedis
+    stopWatchdog
+}
+main
