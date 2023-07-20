e72829ca277c61f64e95783a169a628432f834cf
cwilliams-intelerad <81389845+cwilliams-intelerad@users.noreply.github.com>
2023-07-20 11:40:59 -0500

    PLT-1353: make principals optional; add features (#150)
    ## Submission type
    <!--- Issue Type -->
    - [] Bug fix/cleanup (non-breaking change which fixes known
    deficiencies)
    - [x] New feature (non-breaking change which adds functionality)
    - [] Breaking change (fix or feature that would cause existing
    functionality to change)
    
    #Please verify there is a corresponding jira task for this commit.

--- aws/iam-policy-doc/data.tf
+++ aws/iam-policy-doc/data.tf
@@ -3,18 +3,33 @@ data "aws_iam_policy_document" "main" {
     for_each = var.statements
     content {
       sid    = statement.value.sid
-      effect = lookup(statement.value, "effect", "Allow")
+      effect = try(statement.value.effect, "Allow")
       dynamic "principals" {
-        for_each = statement.value.principals
+        for_each = try(statement.value.principals, {})
         content {
-          type        = lookup(principals.value, "type", "AWS")
+          type        = try(principals.value.type, "AWS")
           identifiers = principals.value.identifiers
         }
       }
-      actions       = lookup(statement.value, "actions", [])
-      not_actions   = lookup(statement.value, "not_actions", [])
-      resources     = lookup(statement.value, "resources", [])
-      not_resources = lookup(statement.value, "resources", [])
+      dynamic "not_principals" {
+        for_each = try(statement.value.not_principals, {})
+        content {
+          type        = try(not_principals.value.type, "AWS")
+          identifiers = not_principals.value.identifiers
+        }
+      }
+      actions       = try(statement.value.actions, [])
+      not_actions   = try(statement.value.not_actions, [])
+      resources     = try(statement.value.resources, [])
+      not_resources = try(statement.value.resources, [])
+      dynamic "condition" {
+        for_each = try(statement.value.condition, {})
+        content {
+          test     = condition.value.test
+          values   = condition.value.values
+          variable = condition.value.variable
+        }
+      }
     }
   }
 }
