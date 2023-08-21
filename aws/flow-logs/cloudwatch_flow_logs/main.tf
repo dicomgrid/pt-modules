daed65e5d280dd458d6aca4b94bf5ea3aa4e8b73
acarter-ims <austin.carter@intelerad.com>
2023-08-21 11:05:41 -0500

    adding eni

--- aws/flow-logs/cloudwatch_flow_logs/main.tf
+++ aws/flow-logs/cloudwatch_flow_logs/main.tf
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_log_stream" "log_name" {
 
 
 module "enable_eni_logs" {
-  source                   = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/flow-logs/cloudwatch_flow_logs/enable_eni_logs?ref=1339x2"
+  source                   = "git::ssh://git@github.com/dicomgrid/pt-modules.git//aws/flow-logs/cloudwatch_flow_logs/enable_eni_logs?ref=PLT-1339x2"
   for_each                 = var.enable_eni_logs ? 1 : 0
   iam_role_name            = data.aws_iam_role.existing_role.arn
   log_destination_type     = var.log_destination_type
