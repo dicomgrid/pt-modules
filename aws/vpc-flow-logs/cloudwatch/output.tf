29318a6dd44f53bdf90d5a7fa7947d46466d74e4
acarter-ims <austin.carter@intelerad.com>
2023-09-02 15:43:40 -0500

    removed unit testing demo

---
+++ aws/vpc-flow-logs/cloudwatch/output.tf
@@ -0,0 +1,14 @@
+output "log_group_name" {
+description = "The name of the CloudWatch log group."
+value       = aws_cloudwatch_log_group.log_group.name
+}
+
+output "log_group_arn" {
+description = "The ARN of the CloudWatch log group."
+value       = aws_cloudwatch_log_group.log_group.arn
+}
+
+output "log_stream_name" {
+description = "The name of the CloudWatch log stream."
+value       = aws_cloudwatch_log_stream.log_name.name
+}
