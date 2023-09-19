3308df89d6161bf71612bf54dfc01eaf59a6d7a1
SkylarHoughtonIntelerad <skylar.houghton@intelerad.com>
2023-09-19 17:04:21 -0500

    PLT-1721: adjusted to mount module

---
+++ hashivault/mount/main.tf
@@ -0,0 +1,32 @@
+resource "vault_mount" "main" {
+  count = var.kvv2_enabled ? 1 : 0
+  allowed_managed_keys = var.allowed_managed_keys
+  description          = var.description
+  namespace            = var.namespace
+  options              = { version = "2" }
+  path                 = var.path
+  type                 = "kv"
+}
+
+resource "vault_kv_secret_backend_v2" "main" {
+  count = var.kvv2_enabled ? 1 : 0
+  cas_required         = var.cas_required
+  delete_version_after = var.delete_version_after
+  max_versions         = var.max_versions
+  mount                = vault_mount.main.path
+  namespace            = var.namespace
+}
+
+resource "vault_mount" "main" {
+  count = var.ssh_enabled ? 1 : 0
+  type      = "ssh"
+  namespace = var.namespace
+  path      = var.path
+}
+
+resource "vault_ssh_secret_backend_ca" "main" {
+  count = var.ssh_enabled ? 1 : 0
+  backend   = vault_mount.main.path
+  namespace = var.namespace
+  generate_signing_key = var.generate_signing_key
+}
