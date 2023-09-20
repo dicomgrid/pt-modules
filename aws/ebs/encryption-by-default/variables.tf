f24ffc2f93431612ffb77d850ce5ea35e54e4ee5
cwilliams-intelerad <cody.williams@intelerad.com>
2023-09-20 01:43:03 -0500

    PLT-1755: default ebs ebd to aws kms key

--- aws/ebs/encryption-by-default/variables.tf
+++ aws/ebs/encryption-by-default/variables.tf
@@ -1,5 +1,2 @@
-locals {
-  key_arn = data.aws_kms_key.by_alias.arn
-}
 variable "enabled" { default = true }
-variable "key_alias" { default = "alias/default-ebs-key" }
+variable "key_id" { default = "alias/aws/ebs" }
