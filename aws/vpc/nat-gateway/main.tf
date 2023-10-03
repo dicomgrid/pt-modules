68364b1876caef8ac949970643373706a631f468
cwilliams-intelerad <cody.williams@intelerad.com>
2023-10-02 20:12:10 -0500

    comment optional blocks

--- aws/vpc/nat-gateway/main.tf
+++ aws/vpc/nat-gateway/main.tf
@@ -16,9 +16,9 @@ resource "aws_nat_gateway" "main" {
   allocation_id     = module.eip.id
   connectivity_type = var.connectivity_type
   private_ip        = var.private_ip
-  secondary_allocation_ids  = var.secondary_allocation_ids
-  secondary_private_ip_address_count = var.secondary_private_ip_address_count
-  secondary_private_ip_addresses = var.secondary_private_ip_addresses
+  # secondary_allocation_ids  = var.secondary_allocation_ids
+  # secondary_private_ip_address_count = var.secondary_private_ip_address_count
+  # secondary_private_ip_addresses = var.secondary_private_ip_addresses
   subnet_id         = var.subnet_id
   tags = var.tags
 }
