c895891eec4fa1e75db3e602ad5e4f570b779c98
cwilliams-intelerad <cody.williams@intelerad.com>
2023-11-02 17:52:32 -0500

    Reupdate policies

--- aws/common/policies/support-billing.tf
+++ aws/common/policies/support-billing.tf
@@ -1,38 +1,46 @@
 data "aws_iam_policy_document" "support_billing" {
 
+  # https://aws.amazon.com/blogs/aws-cloud-financial-management/changes-to-aws-billing-cost-management-and-account-consoles-permissions/
   statement {
-    sid       = "SupportBilling"
+    sid       = "Billing"
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "account:GetAccountInformation",
+      "billing:Get*",
+      "ce:Describe*",
+      "ce:Get*",
+      "ce:List*",
+      "consolidatedbilling:Get*",
+      "consolidatedbilling:List*",
+      "cur:Get*",
+      "cur:Validate*",
+      "freetier:Get*",
+      "invoicing:Get*",
+      "invoicing:List*",
+      "payments:Get*",
+      "payments:List*",
+      "support:*",
+      "tax:Get*",
+      "tax:List*",
+    ]
+  }
+
+  statement {
+    sid       = "BillingLegacy"
     effect    = "Allow"
     resources = ["*"]
     actions = [
       "aws-portal:ViewBilling",
       "aws-portal:ViewUsage",
-      "support:*",
     ]
   }
 
-  # https://aws.amazon.com/blogs/aws-cloud-financial-management/changes-to-aws-billing-cost-management-and-account-consoles-permissions/
   statement {
-    sid       = "SupportBillingPendingMigration"
+    sid       = "Support"
     effect    = "Allow"
     resources = ["*"]
     actions = [
-      "ce:Get*",
-      "ce:Describe*",
-      "ce:List*",
-      "account:GetAccountInformation",
-      "billing:Get*",
-      "payments:List*",
-      "payments:Get*",
-      "tax:List*",
-      "tax:Get*",
-      "consolidatedbilling:Get*",
-      "consolidatedbilling:List*",
-      "invoicing:List*",
-      "invoicing:Get*",
-      "cur:Get*",
-      "cur:Validate*",
-      "freetier:Get*",
       "support:*",
     ]
   }
