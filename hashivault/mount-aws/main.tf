b7ad27beeeebbe016a32bb8f8ecc48a18f9e0f50
Pavel B <pbulygin@ambrahealth.com>
2023-12-12 16:22:50 +0000

    PLT-1760: Rework mount-aws module

--- hashivault/mount-aws/main.tf
+++ hashivault/mount-aws/main.tf
@@ -1,5 +1,5 @@
 
-resource "vault_aws_secret_backend" "aws" {
+resource "vault_aws_secret_backend" "main" {
   access_key                = var.access_key
   secret_key                = var.secret_key
   region                    = var.region
@@ -10,10 +10,12 @@ resource "vault_aws_secret_backend" "aws" {
   path                      = var.path
 }
 
-
 resource "vault_aws_secret_backend_role" "main" {
-  backend         = vault_aws_secret_backend.aws.path
-  name            = var.name
-  credential_type = var.credential_type
-  role_arns       = var.role_arns
+  for_each = var.roles
+
+  backend         = vault_aws_secret_backend.main.path
+  name            = roles.value["name"]
+  credential_type = roles.value["credential_type"]
+  role_arns       = roles.value["role_arns"]
+  policy_document = roles.value["policy_document"]
 }
--- hashivault/mount-aws/variables.tf
+++ hashivault/mount-aws/variables.tf
@@ -1,13 +1,23 @@
 variable "access_key" { default = "" }
 variable "secret_key" { default = "" }
 variable "region" { default = "" }
-variable "credential_type" { default = "assumed_role" }
+# variable "credential_type" { default = "assumed_role" }
 variable "default_lease_ttl_seconds" { default = 3600 }
 variable "description" { default = "" }
 variable "max_lease_ttl_seconds" { default = 7200 }
-variable "name" { default = "" }
+# variable "name" { default = "" }
 variable "namespace" { default = null }
 variable "path" { default = "aws" }
-variable "policy_document" { default = {} }
-variable "role_arns" { default = [] }
-variable "type" { default = "aws" }
+# variable "policy_document" { default = {} }
+# variable "role_arns" { default = [] }
+# variable "type" { default = "aws" }
+
+variable "roles" {
+  default = []
+  type = list(object({
+    credential_type = string
+    name            = string
+    role_arns       = list(string)
+    policy_document = optional(map())
+  }))
+}
