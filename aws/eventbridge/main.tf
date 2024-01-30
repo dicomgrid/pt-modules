9355671b2d2955532893ed02b5207dc7cdc72780
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-01-30 16:51:39 -0600

    PLT-46: variabilized eventbridge rule

--- aws/eventbridge/main.tf
+++ aws/eventbridge/main.tf
@@ -1,15 +1,7 @@
 resource "aws_cloudwatch_event_rule" "eventbridge_rule" {
   name           = var.rule_name
   event_bus_name = var.eventbus_name
-  event_pattern  = <<EOF
-{
-  "source": ["aws.ssm"],
-  "detail-type": ["Maintenance Window Task Execution State-change Notification"],
-  "detail": {
-    "status": ["FAILED"]
-  }
-}
-EOF
+  event_pattern  = var.event_pattern
 }
 
 resource "aws_cloudwatch_event_target" "eventbridge_target" {
