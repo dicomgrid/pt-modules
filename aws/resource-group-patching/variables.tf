2b30815422e3d63e53ab8dcd4efda691e02a09aa
Mike DeMore <mike.demore@intelerad.com>
2024-02-20 17:09:29 -0600

    PLT-2497: slight updates for patching templates/fmt

---
+++ aws/resource-group-patching/variables.tf
@@ -0,0 +1,11 @@
+variable "tags" {
+  type        = map(string)
+  description = "Resource Group Tags"
+  default = {
+    "client_code" = "",
+    "Platform Type"    = ""
+  }
+}
+
+variable "resource_group" {}
+
--- aws/ssm-maintenance-window/variables.tf
+++ aws/ssm-maintenance-window/variables.tf
@@ -1,6 +1,13 @@
-variable "cutoff" {default = 1}
-variable "duration" {default = 2}
-variable "schedule" {default = ""}
-variable "schedule_timezone" {default = "America/New_York"}
-variable "window_enabled" {default = false}
-variable "window_name" {default = ""}
+variable "cutoff" { default = 1 }
+variable "duration" { default = 2 }
+variable "schedule" { default = "" }
+variable "schedule_timezone" { default = "America/New_York" }
+variable "window_enabled" { default = false }
+variable "window_name" { default = "" }
+variable "tags" {
+  type        = map(string)
+  description = "Maintenance Window Tags"
+  default = {
+    "client_code" = ""
+  }
+}
