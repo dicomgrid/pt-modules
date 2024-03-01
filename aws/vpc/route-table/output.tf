377510382ccef1dae9831ba69f169a4ebf90328e
Cody Williams <cody.williams@intelerad.com>
2024-03-01 11:19:19 -0600

    PLT-1998: fix outputs

--- aws/vpc/route-table/output.tf
+++ aws/vpc/route-table/output.tf
@@ -1,18 +1,18 @@
 output "id" {
-  value = aws_route_table.main.id
+  value = aws_route_table.main[0].id
 }
 output "arn" {
-  value = aws_route_table.main.arn
+  value = aws_route_table.main[0].arn
 }
 output "owner_id" {
-  value = aws_route_table.main.owner_id
+  value = aws_route_table.main[0].owner_id
 }
 output "tags_all" {
-  value = aws_route_table.main.tags_all
+  value = aws_route_table.main[0].tags_all
 }
 output "vpc_id" {
-  value = aws_route_table.main.vpc_id
+  value = aws_route_table.main[0].vpc_id
 }
 output "propagating_vgws" {
-  value = aws_route_table.main.propagating_vgws
+  value = aws_route_table.main[0].propagating_vgws
 }
