c0d47235af4daf372aadc4cfab7bf5e25df390a6
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-03-01 12:13:09 -0600

    adjusted archive_file vars

--- aws/lambda/main.tf
+++ aws/lambda/main.tf
@@ -1,8 +1,8 @@
 data "archive_file" "lambda_archive" {
   count       = var.function_code == null ? 0 : 1
   type        = "zip"
-  source_file = "${path.module}/../lambda-tools/${var.archive_file}"
-  output_path = var.source_code_filename
+  source_file = "${path.module}/../lambda-tools/${var.function_code}"
+  output_path = var.archive_file
 }
 
 resource "aws_lambda_function" "lambda_function" {
