097b7c399ecb7d083cd2d2b1ac802e28c9143110
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-03-01 11:25:44 -0600

    removed output var

--- aws/lambda/variables.tf
+++ aws/lambda/variables.tf
@@ -4,7 +4,6 @@ variable "function_name" {}
 variable "handler" {}
 variable "source_code_filename" {}
 variable "environment_variables" {}
-variable "output_path" {}
 variable "role" {}
 variable "timeout" { default = 3 }
 variable "runtime" { default = "" }
