424897fa88d5cdeadb9b79e5c319a9eb8b65a11f
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-03-01 12:59:37 -0600

    added tags to eventbridge

--- aws/eventbridge/variables.tf
+++ aws/eventbridge/variables.tf
@@ -2,3 +2,4 @@ variable "lambda_arn" { type = string }
 variable "eventbus_name" { default = null }
 variable "rule_name" { type = string }
 variable "event_pattern" { default = "" }
+variable "tags" { default = null }
