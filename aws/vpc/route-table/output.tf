0bbf9d1663a81605e3d096e21fcf82229aac34ea
Cody Williams <cody.williams@intelerad.com>
2024-03-01 11:27:14 -0600

    PLT-1998: make outputs optional

--- aws/vpc/route-table/output.tf
+++ aws/vpc/route-table/output.tf
@@ -1,18 +1,18 @@
 output "id" {
-  value = aws_route_table.main[0].id
+  value = try(aws_route_table.main[0].id, null)
 }
 output "arn" {
-  value = aws_route_table.main[0].arn
+  value = try(aws_route_table.main[0].arn, null)
 }
 output "owner_id" {
-  value = aws_route_table.main[0].owner_id
+  value = try(aws_route_table.main[0].owner_id, null)
 }
 output "tags_all" {
-  value = aws_route_table.main[0].tags_all
+  value = try(aws_route_table.main[0].tags_all, null)
 }
 output "vpc_id" {
-  value = aws_route_table.main[0].vpc_id
+  value = try(aws_route_table.main[0].vpc_id, null)
 }
 output "propagating_vgws" {
-  value = aws_route_table.main[0].propagating_vgws
+  value = try(aws_route_table.main[0].propagating_vgws, null)
 }
