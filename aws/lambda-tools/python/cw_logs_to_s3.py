a50fbedf4dcd15ce04f4eeced2d15043866efe62
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-03-08 09:32:04 -0600

    basic cleanup

--- aws/lambda-tools/python/cw_logs_to_s3.py
+++ aws/lambda-tools/python/cw_logs_to_s3.py
@@ -4,35 +4,27 @@ from datetime import datetime, timedelta
 
 #TODO: Convert prints to logging paths
 
-# initialize clients
-# aws_profile_name = 'ims-platform-dev'
-# region = 'us-east-2'
-# session = boto3.Session(profile_name=aws_profile_name)
-
-# logs = session.client('logs', region_name=region)
-# s3 = session.resource('s3', region_name=region)
-# ssm = session.client('ssm', region_name=region)
-
 logs = boto3.client('logs')
 s3 = boto3.resource('s3')
 ssm = boto3.client('ssm')
 
+
 def cw_logs_to_s3(event, context):
     # global default vars
-    bucket_name='plt-test-cw-log-export'
+    bucket_name = 'plt-test-cw-log-export'
     export_to_time = int(round(time.time() * 1000))
     extra_args = {}
     log_groups = []
     log_groups_to_export = []
     log_timestamp = '%Y-%m-%d %H:%M:%S'
-    ssm_parameter_base='/cw_logs_to_s3_last_export/'
+    ssm_parameter_base = '/cw_logs_to_s3_last_export/'
     ssm_value = 0
 
     # Gather log groups with ExportToS3 tags set to true
     while True:
         describe_log_groups = logs.describe_log_groups(**extra_args)
         log_groups = log_groups + describe_log_groups['logGroups']
-        
+
         if not 'nextToken' in describe_log_groups:
             break
         extra_args['nextToken'] = describe_log_groups['nextToken']
@@ -41,13 +33,14 @@ def cw_logs_to_s3(event, context):
 
         log_group_name = log_group['logGroupName']
 
-
         list_tags_log_group = logs.list_tags_log_group(logGroupName=log_group_name)
         log_group_tags = list_tags_log_group['tags']
 
         if 'ExportToS3' in log_group_tags and log_group_tags['ExportToS3'] == 'true':
             timestamp = datetime.now().strftime(log_timestamp)
-            print(f'[{timestamp}] Identified log group {log_group_name} with ExportToS3 tag status: {log_group_tags["ExportToS3"]}')
+            print(
+                f'[{timestamp}] Identified log group {log_group_name} with ExportToS3 tag status: {log_group_tags["ExportToS3"]}'
+            )
             log_groups_to_export.append(log_group_name)
             timestamp = datetime.now().strftime(log_timestamp)
             print(f'[{timestamp}] --> Log group {log_group_name} added to export list.')
@@ -57,9 +50,14 @@ def cw_logs_to_s3(event, context):
         log_group_arn_raw = logs.describe_log_groups(logGroupNamePrefix=log_group_name)
         log_group_arn = log_group_arn_raw['logGroups'][0]['arn']
         account_id = log_group_arn.split(":")[4]
-        ssm_parameter_name = ("%s%s" % (ssm_parameter_base, log_group_name)).replace("//", "/") 
-        
-        #https://stackoverflow.com/a/71307116
+        ssm_parameter_name = ("%s%s" %
+                              (ssm_parameter_base, log_group_name)).replace("//", "/")
+
+        '''
+        taskId hardcoded in export path, see https://stackoverflow.com/a/71307116
+        technique to export to temporary path, then export to finalized formatted path
+        Would align with the pd method provided by Azure Sentinel
+        '''
         destination_prefix = f"{account_id}{log_group_name}"
 
         try:
@@ -67,16 +65,20 @@ def cw_logs_to_s3(event, context):
             ssm_value = get_timestamp['Parameter']['Value']
 
             timestamp = datetime.now().strftime(log_timestamp)
-            print(f'[{timestamp}] Log group {log_group_name} existing timestamp found: {ssm_value}.')
+            print(
+                f'[{timestamp}] Log group {log_group_name} existing timestamp found: {ssm_value}.'
+            )
         except ssm.exceptions.ParameterNotFound:
             timestamp = datetime.now().strftime(log_timestamp)
-            print(f'[{timestamp}] Log group {log_group_name} timestamp does not exist, using default value.')
+            print(
+                f'[{timestamp}] Log group {log_group_name} timestamp does not exist, using default value.'
+            )
 
         if export_to_time - int(ssm_value) < (24 * 60 * 60 * 1000):
             # Haven't been 24hrs from the last export of this log group
             print(f'[{timestamp}] Skipped until 24hrs from last export is completed')
             continue
-        
+
     # Execute log export
         create_export_task = logs.create_export_task(
             logGroupName=log_group_name,
@@ -90,27 +92,29 @@ def cw_logs_to_s3(event, context):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f'[{timestamp}] --> Export task {export_task_id} created.')
 
-        while True:
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            describe_export_task = logs.describe_export_tasks(taskId=export_task_id)
-            status = describe_export_task['exportTasks'][0]['status']['code']
+        # Removing this loop, the export task is a very serial async operation and cannot kick off subsequent jobs until the prior completes
+        # Best to reinvoke the lambda for additional log groups than waiting for completetion, max 900 second timeout
+        # while True:
+        #     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        #     describe_export_task = logs.describe_export_tasks(taskId=export_task_id)
+        #     status = describe_export_task['exportTasks'][0]['status']['code']
 
-            if status == 'PENDING':
-                print(f'[{timestamp}] Export task {export_task_id} for {log_group_name} still waiting to run...')
-                time.sleep(5)
-                continue
-            elif status == 'RUNNING':
-                print(f'[{timestamp}] Export task {export_task_id} for {log_group_name} running...')
-                time.sleep(5)
-                continue
-            elif status == 'COMPLETED':
-                print(f'[{timestamp}] --> Export task {export_task_id} completed successfully')
-                break
-            elif status == 'FAILED' or status == 'CANCELLED':
-                print(f'[{timestamp}] Export task {export_task_id} failed or was cancelled.')
-                break
+        #     if status == 'PENDING':
+        #         print(f'[{timestamp}] Export task {export_task_id} for {log_group_name} still waiting to run...')
+        #         time.sleep(5)
+        #         continue
+        #     elif status == 'RUNNING':
+        #         print(f'[{timestamp}] Export task {export_task_id} for {log_group_name} running...')
+        #         time.sleep(5)
+        #         continue
+        #     elif status == 'COMPLETED':
+        #         print(f'[{timestamp}] --> Export task {export_task_id} completed successfully')
+        #         break
+        #     elif status == 'FAILED' or status == 'CANCELLED':
+        #         print(f'[{timestamp}] Export task {export_task_id} failed or was cancelled.')
+        #         break
 
-    # Update export timestamp
+        # Update export timestamp
         update_timestamp = ssm.put_parameter(
             Name=ssm_parameter_name,
             Type="String",
@@ -118,59 +122,14 @@ def cw_logs_to_s3(event, context):
             Overwrite=True
         )
         timestamp = datetime.now().strftime(log_timestamp)
-        print(f'[{timestamp}] --> Log group timestamp updated to {export_to_time}: {update_timestamp["ResponseMetadata"]["HTTPStatusCode"]} {update_timestamp["ResponseMetadata"]["RequestId"]}')
-
+        print(
+            f'[{timestamp}] --> Log group timestamp updated to {export_to_time}: {update_timestamp["ResponseMetadata"]["HTTPStatusCode"]} {update_timestamp["ResponseMetadata"]["RequestId"]}'
+        )
 
 
 ###
 ### Left for historical knowledge, found from https://github.com/Azure/Azure-Sentinel/blob/master/DataConnectors/AWS-S3/CloudWatchLambdaFunction.py
 ###
-# def lambda_handler(event, context):
-#     """
-#     The function gets data from cloud watch and put it in the desired bucket in the required format for Sentinel.
-#     :param event: object that contains information about the current state of the execution environment.
-#     :param context: object that contains information about the current execution context.
-#     """
-#     try:
-# START_TIME_UTC = ''
-# END_TIME_UTC = ''
-# DAYS_RANGE = 0
-# OUTPUT_FILE_NAME='test'
-# bucket_name='plt-test-cw-log-export'
-# BUCKET_PREFIX='test/'
-# LOG_GROUP_NAME='/aws/eks/plt-test-eks/cluster'
-# LOG_STREAM_NAME='kube-apiserver-audit-7126d400dbd6ab4ddaa3d7d585e42ce1'
-
-# if START_TIME_UTC == '':
-#     current_time_raw = datetime.utcnow()
-#     current_time = current_time_raw.strftime('%m-%d-%Y %H:%M:%S')
-#     current_time_log = delta_time_raw.strftime('%Y-%m-%dT%H:%M:%S.%f')
-#     START_TIME_UTC = datetime.strptime(current_time, '%m-%d-%Y %H:%M:%S')
-
-# if END_TIME_UTC == '':
-#     delta_time_raw = current_time_raw - timedelta(days=DAYS_RANGE)
-#     delta_time = delta_time_raw.strftime('%m-%d-%Y %H:%M:%S')
-#     delta_time_log = delta_time_raw.strftime('%Y-%m-%dT%H:%M:%S.%f')
-#     END_TIME_UTC = datetime.strptime(delta_time, '%m-%d-%Y %H:%M:%S')
-
-#         unix_start_time = int(time.mktime(START_TIME_UTC.timetuple())) * 1000
-#         unix_end_time = int(time.mktime(END_TIME_UTC.timetuple())) * 1000
-#         TIMESTAMP = ('%s-%s' % (unix_start_time, unix_end_time))
-
-#         if unix_start_time <= unix_end_time:
-#             log_events = logs.get_log_events(
-#                 logGroupName=LOG_GROUP_NAME,
-#                 logStreamName=LOG_STREAM_NAME,
-#                 startTime=unix_start_time
-#             )
-
-#         if unix_start_time <= unix_end_time:
-#             log_events = logs.get_log_events(
-#                 logGroupName=LOG_GROUP_NAME,
-#                 logStreamName=LOG_STREAM_NAME,
-#                 startTime=unix_end_time,
-#                 endTime=unix_start_time
-#             )
 
 #         # Convert events to json object
 #         json_string = json.dumps(log_events)
