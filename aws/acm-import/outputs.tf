7595446e3b8b980447911813e722b235f84b7ac3
Andrew K <andrew.knighton@intelerad.com>
2024-04-02 13:14:32 -0400

    PLT-2081: adding outputs

--- aws/acm-import/outputs.tf
+++ aws/acm-import/outputs.tf
@@ -1,5 +1,7 @@
-# output "cert_test" {
-#   value       = data.vault_kv_secret_v2.cert.data["nexus_user"]
-#   description = "description"
-#   sensitive   = true
-# }
+output "acm_arn" {
+  value = aws_acm_certificate.main.arn
+}
+
+output "acm_domain" {
+  value = aws_acm_certificate.main.domain_name
+}
