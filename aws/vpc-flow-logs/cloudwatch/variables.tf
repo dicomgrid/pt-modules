284ec4766127634ffb2e33966eb519b596878057
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-04-05 15:32:35 -0500

    PLT-2728: made log_destination dynamic

--- aws/vpc-flow-logs/cloudwatch/variables.tf
+++ aws/vpc-flow-logs/cloudwatch/variables.tf
@@ -1,42 +1,14 @@
 variable "account_id" {}
-
+variable "enable-subnet-logs" { default = false }
+variable "enable-vpc-logs" { default = false }
+variable "enable-eni-logs" { default = false }
+variable "enable-tgw-attachment-logs" { default = false }
 variable "region" {}
-
 variable "vpc_id" {}
-
-
 variable "log_destination" {}
 variable "traffic_type" {}
-
 variable "max_aggregation_interval" {}
-
-variable "retention_in_days" {
-  type    = number
-  default = 180
-}
-variable "log_destination_type" {
-  type    = string
-  default = "cloud-watch-logs"
-}
-variable "tags" {
-  description = "A map of tags to assign to the resource."
-  type        = map(string)
-  default     = {}
-}
-variable "iam_role_arn" {
-  type = string
-}
-
-variable "enable-subnet-logs" {
-
-}
-variable "enable-vpc-logs" {
-
-}
-variable "enable-eni-logs" {
-
-}
-
-variable "enable-tgw-attachment-logs" {
-
-}
+variable "retention_in_days" { default = 180 }
+variable "log_destination_type" { default = "cloud-watch-logs" }
+variable "tags" {}
+variable "iam_role_arn" {}
