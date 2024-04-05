538f4eeed77f7b367f5acaa24311338eeb1286ed
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-04-05 16:26:45 -0500

    conditional output

--- aws/vpc-flow-logs/output.tf
+++ aws/vpc-flow-logs/output.tf
@@ -1,14 +1,14 @@
 output "log_group_name" {
 description = "The name of the CloudWatch log group."
-value       = aws_cloudwatch_log_group.log_group[0].name
+value       = var.log_destination_type == "cloud-watch-logs" ? aws_cloudwatch_log_group.log_group[0].name : null
 }
 
 output "log_group_arn" {
 description = "The ARN of the CloudWatch log group."
-value       = aws_cloudwatch_log_group.log_group[0].arn
+value       = var.log_destination_type == "cloud-watch-logs" ? aws_cloudwatch_log_group.log_group[0].arn : null
 }
 
 output "log_stream_name" {
 description = "The name of the CloudWatch log stream."
-value       = aws_cloudwatch_log_stream.log_name[0].name
+value       = var.log_destination_type == "cloud-watch-logs" ? aws_cloudwatch_log_group.log_stream[0].name : null
 }
