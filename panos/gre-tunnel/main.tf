e3363169050c64ffc7cae36cf2b2ea7c269b17d0
Cody Williams <cody.williams@intelerad.com>
2024-04-17 09:15:36 -0500

    PLT-2785: add bgp auth & bugfixes

--- panos/gre-tunnel/main.tf
+++ panos/gre-tunnel/main.tf
@@ -1,25 +1,25 @@
-resource "panos_tunnel_interface" "main" {
-  count = try(var.tunnel_interface.create, true) ? 1 : 0
+# resource "panos_tunnel_interface" "main" {
+#   count = try(var.tunnel_interface.create, true) ? 1 : 0
 
-  name               = try(var.tunnel_interface.name, var.tunnel_interface)
-  vsys               = try(var.tunnel_interface.vsys, "vsys1")
-  comment            = try(var.tunnel_interface.comment, "Tunnel interface for GRE tunnel - ${var.name}")
-  netflow_profile    = try(var.tunnel_interface.netflow_profile, null)
-  static_ips         = try(var.tunnel_interface.static_ips, [])
-  management_profile = try(var.tunnel_interface.management_profile, null)
-  mtu                = try(var.tunnel_interface.mtu, null)
+#   name               = var.tunnel_interface.name
+#   vsys               = try(var.tunnel_interface.vsys, "vsys1")
+#   comment            = try(var.tunnel_interface.comment, "Tunnel interface for GRE tunnel - ${var.name}")
+#   netflow_profile    = try(var.tunnel_interface.netflow_profile, null)
+#   static_ips         = try(var.tunnel_interface.static_ips, [])
+#   management_profile = try(var.tunnel_interface.management_profile, null)
+#   mtu                = try(var.tunnel_interface.mtu, null)
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
 resource "panos_gre_tunnel" "main" {
   name                  = var.name
   interface             = var.interface
   local_address_value   = var.local_address
   peer_address          = var.peer_address
-  tunnel_interface      = try(panos_tunnel_interface.main[0].name, var.tunnel_interface.name, var.tunnel_interface)
+  tunnel_interface      = var.tunnel_interface #try(panos_tunnel_interface.main[0].name, var.tunnel_interface.name, var.tunnel_interface)
   ttl                   = var.ttl
   copy_tos              = var.copy_tos
   enable_keep_alive     = var.enable_keep_alive
