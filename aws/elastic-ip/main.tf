8343b2609be183a0c9a5f9ce22c56a77f1c1a367
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-04-29 14:17:44 -0500

    fmt

--- aws/elastic-ip/main.tf
+++ aws/elastic-ip/main.tf
@@ -1,15 +1,15 @@
 resource "aws_eip" "main" {
   public_ipv4_pool     = var.byoip_pool == true ? "ipv4pool-ec2-03f7aa330fe7e27cc" : ""
-  domain = var.domain
+  domain               = var.domain
   network_border_group = var.network_border_group
 
   tags = merge({
-    Application = var.Application
-    Billing     = var.Billing
-    Creator     = var.Creator
-    Environment = var.Environment
-    Name        = var.Name
-    Owner       = var.Owner
+    Application              = var.Application
+    Billing                  = var.Billing
+    Creator                  = var.Creator
+    Environment              = var.Environment
+    Name                     = var.Name
+    Owner                    = var.Owner
     aws-migration-project-id = var.aws_project_id
     map-migrated             = var.map_migrated
   }, var.tags)
