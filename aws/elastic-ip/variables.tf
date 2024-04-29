8a1ffa67124b9c3c96bb1a40121a76f6f23158a8
SkylarHoughtonGithub <skylar.houghton@intelerad.com>
2024-04-29 13:30:55 -0500

    capital vars

--- aws/elastic-ip/variables.tf
+++ aws/elastic-ip/variables.tf
@@ -1,14 +1,14 @@
-variable "application" { default = "default" }
+variable "Application" { default = "default" }
 variable "aws_project_id" { default = null }
-variable "billing" { default = "" }
+variable "Billing" { default = "" }
 variable "byoip_pool" { default = false }
-variable "creator" { default = "terraform" }
+variable "Creator" { default = "terraform" }
 variable "domain" { default = "vpc" }
-variable "environment" {}
+variable "Environment" {}
 variable "instance_id" {}
 variable "map_migrated" { default = null }
-variable "name" {}
-variable "owner" { default = "platform" }
+variable "Name" {}
+variable "Owner" { default = "platform" }
 
 # Added by Pavel to accomodate Houston LZ EIP allocation
 variable "network_border_group" { default = null }
