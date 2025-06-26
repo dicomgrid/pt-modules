cf8cdeffe90f44819d7c5d4638a64b6b354b2957
austin-carter_ims <austin.carter@intelerad.com>
2025-06-26 05:18:37 -0500

    PLT-1: BREAKING: test

---
+++ aws/ami-copy/variables.tf
@@ -0,0 +1,18 @@
+variable "amis_to_copy" {
+  description = "A list of AMIs and their details to be copied"
+  type = list(object({
+    source_ami_id         = string
+    source_ami_region     = string
+    target_ami_name       = string
+    target_ami_description= string
+    target_region         = string
+    encrypted             = bool
+    kms_key_id            = string
+    tags                  = map(string)
+  }))
+}
+variable "subnets_filter" {
+  description = "Filter to apply to subnets when looking up subnet ids"
+  type        = string
+  default     = "private"
+}
