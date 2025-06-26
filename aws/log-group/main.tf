12bf67b20321819e83cca5701317674d4943e8f9
austin-carter_ims <austin.carter@intelerad.com>
2025-06-26 05:07:37 -0500

    PLT-1111: breaking: testing breaking change

---
+++ aws/log-group/main.tf
@@ -0,0 +1,10 @@
+resource "aws_cloudwatch_log_group" "main" {
+  name              = local.tags.Name
+  skip_destroy      = var.skip_destroy
+  retention_in_days = var.retention_in_days
+  kms_key_id        = var.kms_key_id
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags              = local.tags
+}
