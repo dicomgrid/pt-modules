bbc8447efc689d30beeeb6053491f1d2a0b04b72
austin-carter_ims <austin.carter@intelerad.com>
2025-07-04 19:56:34 -0500

    PLT-1: fix modules test

--- aws/acm/main.tf
+++ aws/acm/main.tf
@@ -29,3 +29,4 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.record : record.fqdn]
 }
+##change
